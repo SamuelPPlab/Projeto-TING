d7783486400479cf6062bb63fc86bf74df0813c6
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-08-17 15:54:48 -0300

    req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,32 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if not instance:
+        lines_from_file = txt_importer(path_file)
+        data = {
+            "nome do arquivo": path_file,
+            "qtd_linhas": 3,
+            "linhas_do_arquivo": lines_from_file
+        }
+
+        for file in instance.data:
+            if file["nome do arquivo"] == path_file:
+                return file
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance:
+        return print("Não há elementos", file=sys.stdout)
+    
+    removed_file = instance.dequeue()['nome do arquivo']
+    print(f"Arquivo {removed_file} removido com sucesso", file=sys.stdout)
+
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return instance.search(position)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
