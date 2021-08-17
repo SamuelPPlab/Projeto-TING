df481a29f9df1acef16faed847e6237fee3451c2
Raphael Caputo <faelelf2@gmail.com>
2021-08-17 14:07:44 -0300

    É Hora de dar tchau?!"

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,6 +1,6 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        """Inicialize sua estrutura aqui, Somente para abrir a PR"""
 
     def __len__(self):
         """Aqui irá sua implementação"""
