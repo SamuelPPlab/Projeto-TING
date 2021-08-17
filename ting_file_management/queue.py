c347f0257f28bf777c6ad192d227bca359de5415
renatomak <renato.mark.silva@gmail.com>
2021-08-17 18:38:38 -0300

    requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
 class Queue:
+
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self._data:
+            return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        try:
+            if index == -1:
+                raise
+            return self._data[index]
+        except:
+            print('O erro IndexError foi lançado')
+            raise IndexError
