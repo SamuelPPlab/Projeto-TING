94ceffd82441b3d46f85bc429756d864e0435ad1
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2021-08-17 17:41:32 -0300

    last projeeect

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,29 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+def process(path_file, instance):
+    if len(instance) < 1:
+        file = txt_importer(path_file)
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+        sys.stdout.write(str(result))
+        instance.enqueue(result)
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    file_length = len(instance)
+    if file_length <= 0:
+        return sys.stdout.write("Não há elementos\n")
+    removed_file = instance.dequeue()['nome_do_arquivo']
+    sys.stdout.write(f"Arquivo {removed_file} removido com sucesso\n")
+
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        search_result = instance.search(position)
+        return sys.stdout.write(str(search_result))
+    except IndexError:
+        sys.stderr.write("Posição inválida")
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
+from ting_file_management.queue import Queue
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    list = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        searched_words = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                searched_words["ocorrencias"].append({'linha': index + 1})
+            if len(searched_words["ocorrencias"]):
+                list.append(searched_words)
+
+    return list
 
 
 def search_by_word(word, instance):
