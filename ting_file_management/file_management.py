55a10da2775b226010601d8ac49974856aee0d28
nevesfreire <nando.neves@gmail.com>
2021-08-17 19:04:24 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if '.txt' in path_file:
+            with open(path_file) as txt_File:
+                if txt_File:
+                    news = txt_File.read().splitlines()
+                    return list(news)
+                sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        sys.stderr.write("Formato inválido\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
