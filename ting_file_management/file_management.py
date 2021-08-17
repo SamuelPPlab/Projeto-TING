f63bcc6646dd0797c7ec33aa6c058cf07317a0b3
CleberFontinele <cleberfontinele2387@gmail.com>
2021-08-17 18:22:00 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+# O splitlines()método divide uma string em uma lista.
+# A divisão é feita nas quebras de linha.
+# https://www.delftstack.com/pt/howto/python/python-print-to-stderr/
+# try,except https://www.youtube.com/watch?v=RHSxIKGCX7c
