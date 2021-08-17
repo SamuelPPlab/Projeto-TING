c496022a6df6aa73b0406d3a76755febd02c4a9c
Francisco <franciscoliratorres@gmail.com>
2021-08-17 17:45:43 -0300

    add parte1 req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    my_data = dict()
+    data_len = len(txt_importer(path_file))
+    my_data["nome_do_arquivo"] = path_file
+    my_data["qtd_linhas"] = data_len
+    my_data["linhas_do_arquivo"] = txt_importer(path_file)
+    # https://stackoverflow.com/questions/4547274/convert-a-python-dict-to-a-string-and-back
+    to_str = str(my_data)
+    # https://www.geeksforgeeks.org/sys-stdout-write-in-python/
+    return sys.stdout.write(to_str)
 
 
 def remove(instance):
@@ -8,3 +20,7 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+# teste = process("statics/arquivo_teste.txt", "oi")
+# print(teste)
