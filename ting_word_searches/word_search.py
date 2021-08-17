bb3f259671e114ec52355e9900ecf1124fa7fb78
Pablo Pessanha <pablopessanh@gmail.com>
2021-08-17 14:26:35 -0300

    E TEEETRAAAAAA AAAAA

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words_info = []
 
+    for index in range(instance.__len__()):
+        file = instance.search(index)
+        lines = file["linhas_do_arquivo"]
+        occurrences = []
+
+        count = 0
+        for line in lines:
+            count += 1
+            if word.lower() in line.lower(): occurrences.append({'linha': count})
+
+        if len(occurrences) > 0:
+            words_info.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return words_info
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
