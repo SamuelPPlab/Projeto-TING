0ff91f41e94f29127042a7d56b25aa21f1769e2d
hvenses <hvenses@gmail.com>
2021-08-17 16:09:53 -0300

    Commitando requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            # https://www.w3schools.com/python/ref_string_splitlines.asp
+            text_lines = file.read().splitlines()
+            return text_lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
