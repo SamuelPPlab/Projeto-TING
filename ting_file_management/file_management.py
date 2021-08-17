d538a0fffa06e7e3198e2842303f360080af3de1
Emerson Moonlit <emoonlit@pop-os.localdomain>
2021-08-17 17:28:10 -0300

    Queue and file management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,25 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    lines = []
+    try:
+        new_var = check_file_type_in_path(path_file) != "txt"
+        if new_var:
+            sys.stderr.write("Formato inválido\n")
+        else:
+            with open(path_file, "rt", newline="\n") as f:
+                for line in f:
+                    lines.append(line.strip())
+        return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+def check_file_type_in_path(path_file):
+    return path_file.split(".")[1]
+
+
+# if __name__ == "__main__":
+#     print(txt_importer("statics/arquivo_test.txt"))
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,18 @@
+from ting_file_management.deque import Deque
+
+
 class Queue:
-    def __init__(self):
-        """Inicialize sua estrutura aqui"""
+    def __init__(self):  # faz um push dos elementos da fila
+        self.__data = Deque()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.__data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.__data.push_back(value)  # Enfileirar
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.__data.pop_front()  # Desinfileirar
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        return self.__data.find_element(index)
