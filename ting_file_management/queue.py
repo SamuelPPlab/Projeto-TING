68a95712efb466d017ab62c0b908f4b07d304ef4
hvenses <hvenses@gmail.com>
2021-08-17 15:37:22 -0300

    Commitando requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,41 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.data = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self.data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        item = self.data[0]
+        del self.data[0]
+        return item
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        try:
+            if index > len(self.data) or index <= -1:
+                raise IndexError
+            else:
+                # print(self.data[index])
+                return self.data[index]
+        except IndexError:
+            raise IndexError
+
+
+'''Testes manuais:
+run = Queue()
+run.enqueue(1)
+run.enqueue(2)
+run.enqueue(3)
+print(run.data)
+run.dequeue()
+print(run.data)
+run.search(0)
+'''
