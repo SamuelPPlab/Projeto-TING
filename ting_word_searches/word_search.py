005005a3f94fa761dbc4504735944bce5f5a5be6
cezar88augusto <cezar88augusto@gmail.com>
2021-08-17 17:49:01 -0300

    Requisitos 6 e 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,31 @@
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    array = []
+    counter = 0
+    for element in instance._data:
+        obj = {
+            "palavra": word,
+            "arquivo": element["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for _index, line in enumerate(element["linhas_do_arquivo"]):
+            if word in line:
+                counter += 1
+                obj["ocorrencias"].append({"linha": counter})
+                array.append(obj)
+    return array
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    array = []
+    for element in instance._data:
+        obj = {
+            "palavra": word,
+            "arquivo": element["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for index, line in enumerate(element["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                obj["ocorrencias"].append({"linha": index+1, "conteudo": line})
+                array.append(obj)
+    return array
