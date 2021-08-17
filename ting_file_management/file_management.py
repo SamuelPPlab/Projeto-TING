b802956ec4c6cf0a44d7af8f3901ac21a4258ce0
Kesley Lahoz <namielahoz@gmail.com>
2021-08-17 18:27:17 -0300

    file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith("txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file) as file:
+            content = file.read().splitlines()
+            return content
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
