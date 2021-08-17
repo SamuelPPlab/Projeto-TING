53e19600c7895964c30068f7209a632c55eccd2f
kramer <kramerscs@gmail.com>
2021-08-17 15:02:38 -0300

    feat: req5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,4 +1,5 @@
 import sys
+# from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
 
 
@@ -7,19 +8,33 @@ def process(path_file, instance):
     content = txt_importer(path_file)
     current_queue = instance._data
 
-    if content not in current_queue:
-        instance.enqueue(content)
-
     result = f"""'nome_do_arquivo': '{path_file}',
     'qtd_linhas': {len(content)},
     'linhas_do_arquivo': {content}"""
+
+    if result not in current_queue:
+        instance.enqueue(result)
 
     return sys.stdout.write(result)
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    current_queue = instance._data
+    if len(current_queue) == 0:
+        return sys.stdout.write("Não há elementos\n")
+    first_item = instance.dequeue()
+    file_name = first_item.split(',')[0].split(" ")[1][1:-1]
+
+    return sys.stdout.write(f"Arquivo {file_name} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    if position > len(instance._data):
+        return sys.stderr.write("Posição inválida\n")
+    return sys.stdout.write(instance.search(position))
+
+# q = Queue()
+# process("statics/arquivo_teste.txt", q)
+# remove(q)
