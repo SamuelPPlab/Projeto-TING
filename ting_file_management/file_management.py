6bbbb17b73763206a75e5b3de61c2b1e86c74d13
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-08-17 15:57:30 -0300

    requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError()
+        with open(path_file, "r") as file:
+            content = file.read()
+        return content.splitlines()
+    except OSError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    except ValueError:
+        print("Formato inválido", file=sys.stderr)
