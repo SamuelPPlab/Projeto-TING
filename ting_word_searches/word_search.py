e6ada8ea4da4555d35ab9d95368a607cedccdac4
kramer <kramerscs@gmail.com>
2021-08-17 18:42:47 -0300

    feat: req7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,3 +19,17 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    queue = instance._data
+    for item in queue:
+        ocorruncies = []
+        result_obj = {"palavra": word, "arquivo": item["nome_do_arquivo"]}
+        file_lines = item["linhas_do_arquivo"]
+        for index, line in enumerate(file_lines):
+            if word.lower() in line.lower():
+                ocorruncies.append({"linha": index + 1, "conteudo": line})
+    result_obj["ocorrencias"] = ocorruncies
+    if len(ocorruncies) == 0:
+        return []
+    final_result = [result_obj]
+
+    return final_result
