7405901e54ac4bba201dc4cd8be3bc02844cbae7
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-08-17 14:56:04 -0300

    let's go ting

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        if value not in self.data:
+            return self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value = self.data[0]
+        del self.data[0]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.data):
+            return self.data[index]
+        raise IndexError
