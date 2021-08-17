7eb370612a7016a1c5db9c50eadb2ad3c3c1246e
MoisesSantana <moisaant@gmail.com>
2021-08-17 19:53:53 -0300

    req2 ao req5

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if '.txt' in path_file:
+        try:
+            with open(path_file) as file:
+                result = []
+                for row in file:
+                    result.append(row.strip())
+
+                return result
+        except FileNotFoundError:
+            return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+    return sys.stderr.write('Formato inválido\n')
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,37 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if path_file == instance.search(index)["nome_do_arquivo"]:
+            return None
+
+    file_in_arr = txt_importer(path_file)
+    result_qty_row = len(file_in_arr)
+    instance.enqueue({
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": result_qty_row,
+        "linhas_do_arquivo": file_in_arr
+    })
+
+    print(instance.search(len(instance) - 1))
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print('Não há elementos')
+    else:
+        removed_element = instance.dequeue()
+        file_name = removed_element['nome_do_arquivo']
+        print(f'Arquivo {file_name} removido com sucesso')
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    if 0 <= position < len(instance):
+        print(instance.search(position))
+    else:
+        sys.stderr.write('Posição inválida')
