d79faeed4f95624e4e250e10a82be2566447b269
moacyrrln <moacyrrln@gmail.com>
2021-08-17 19:41:41 -0300

    req3 e 4 agora foi

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,51 @@
 import sys
-from ting_file_management.file_management import txt_importer
+
+
+class Queue:
+    def __init__(self):
+        self._data = list()
+
+    def __len__(self):
+        return len(self._data)
+
+    def __str__(self):
+        return "Deque(" + ", ".join(map(lambda x: str(x), self._data)) + ")"
+
+    def enqueue(self, value):
+        self._data.append(value)
+
+    def dequeue(self):
+        if self._data:
+            return self._data.pop(0)
+        return None
+
+    def xablau(self):
+        for item in range(len(self._data)):
+            print(item, self._data[item])
+
+    def search(self, index):
+        if 0 <= index < len(self._data):
+            return self._data[index]
+        else:
+            raise IndexError()
+
+
+def txt_importer(path_file):
+    try:
+        with open(path_file, "r", encoding="utf-8") as file:
+            content_file = []
+            if path_file.split('.')[-1] == 'txt':
+                for readline in file:
+                    content_file.append(readline.rstrip('\n'))
+                return content_file
+            sys.stderr.write("Formato inválido\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
 
 
 def process(path_file, instance):
-    for posicao in range(instance.size()):
-        if path_file == instance.search(posicao)["nome_do_arquivo"]:
+    for index in range(len(instance)):
+        if path_file == instance.search(index)["nome_do_arquivo"]:
             return None
     arquivo = txt_importer(path_file)
     retorno = {
@@ -28,3 +69,8 @@ def file_metadata(instance, position):
         return instance.search(position)
     except IndexError:
         return sys.stderr.write("Posição inválida")
+
+
+project = Queue()
+print(process("statics/arquivo_teste.txt", project))
+print(process("statics/arquivo_teste.txt", project))
