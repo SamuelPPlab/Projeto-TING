dfecdc6e8bfa32178a2036e3d9dded29a7f6dc8f
kramer <kramerscs@gmail.com>
2021-08-17 13:11:56 -0300

    feat: req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,23 @@
+import sys
+
+
 def txt_importer(path_file):
+
     """Aqui irá sua implementação"""
+    file_extension = path_file.split(".")[1]
+    try:
+        if file_extension != "txt":
+            return sys.stderr.write("Formato inválido\n")
+        else:
+            txt_content = []
+            with open(path_file, 'r') as txt_file:
+                file = txt_file.readlines()
+                for line in file:
+                    txt_content.append(line.strip())
+                print(txt_content)
+                return txt_content
+
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        return file
