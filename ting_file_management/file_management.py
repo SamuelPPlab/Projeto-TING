617ff49912a9409754c06ddd34c574f2865a20dc
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2021-08-17 15:43:11 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith("txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file) as f:
+            contents = f.read().splitlines()
+            return contents
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
