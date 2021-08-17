45e047f585e5dd0711260388404a1109db674c1e
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-08-17 15:21:32 -0300

    req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('txt'):
+            sys.stderr.write("Formato Inválido\n")
+        with open(path_file, 'r') as file:
+            text = file.read().splitlines()
+            return text
+    
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
