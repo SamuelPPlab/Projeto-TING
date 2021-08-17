57d51faec33278d41a125dbdfdb4ee3008e18e37
Thays Costa <thayscosta3@gmail.com>
2021-08-17 17:27:09 -0300

    2nd req.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    """
+        - Importa informações de um arquivo .txt, tendo "\n" como separador.
+    """
+
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file) as content_file:
+                return [line for line in content_file.read().strip().split(
+                    "\n")]
+        else:
+            return sys.stderr.write("Formato inválido\n")
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
