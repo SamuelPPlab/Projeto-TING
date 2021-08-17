a8279d0ad58228bf42b35b0ebe961103176822c2
moacyrrln <moacyrrln@gmail.com>
2021-08-17 17:06:12 -0300

    iniciando

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, "r", encoding="utf-8") as file:
+            content_file = []
+            if path_file.split('.')[-1] == 'txt':                
+                for readline in file:
+                    content_file.append(readline.rstrip('\n'))
+                return content_file
+            return "Formato inválido\n"
+    except:
+        return f"Arquivo {path_file} não encontrado\n"
+
+print(txt_importer("statics/arquivo_teste.txt"))
+
+print(txt_importer("statics/arquivo_teste.csv"))
+
+print(txt_importer("statics/arquivo_nao_existe.txt"))
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,27 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
+
+    def __str__(self):
+        return "Deque(" + ", ".join(map(lambda x: str(x), self._data)) + ")"
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self._data:
+            return self._data.pop(0)
+        return None
+
+    def xablau(self):
+        for item in range(len(self._data)):
+            print(item, self._data[item])
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self._data):
+            return self._data[index]
+
+
