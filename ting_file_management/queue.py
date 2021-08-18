2f9cf7bd23a41dd4a217956de8759c76000f3d55
Filipi Firmino <contatoic3web@gmail.com>
2021-08-18 16:49:34 -0300

    fix error lint

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -2,17 +2,14 @@ class Queue:
     def __init__(self):
         self.files = []
 
-
-    def __len__(self):       
+    def __len__(self):
         return len(self.files)
-
 
     def enqueue(self, value):
         return self.files.append(value)
 
-
     def dequeue(self):
-       return self.files.pop(0)
+        return self.files.pop(0)
 
     def search(self, index):
         if index >= 0:
