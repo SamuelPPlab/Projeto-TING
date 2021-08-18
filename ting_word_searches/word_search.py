4e7822018261dedb89d45af6c96137b38ac7f8f8
nevesfreire <nando.neves@gmail.com>
2021-08-18 16:40:00 -0300

    ate 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,37 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    result_list = list()
+    for position in range(0, instance.__len__()):
+        item = instance.search(position)
+        matches = list()
+        for line in item['linhas_do_arquivo']:
+            if word.lower() in line.lower():
+                matches.append({'linha': item['linhas_do_arquivo'].index(line) + 1})
+        if len(matches) > 0:
+            result_list.append({
+                'palavra': word,
+                'arquivo': item['nome_do_arquivo'],
+                'ocorrencias': matches
+            })
+    return result_list
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    result_list = list()
+    for position in range(0, instance.__len__()):
+        item = instance.search(position)
+        matches = list()
+        for line in item['linhas_do_arquivo']:
+            if word.lower() in line.lower():
+                matches.append({
+                    'linha': item['linhas_do_arquivo'].index(line) + 1,
+                    'conteudo': line,
+                })
+        if len(matches) > 0:
+            result_list.append({
+                'palavra': word,
+                'arquivo': item['nome_do_arquivo'],
+                'ocorrencias': matches
+            })
+    return result_list
