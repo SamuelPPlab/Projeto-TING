3b371ab87bac20ede9ff73c3b5db1802e3423091
dennerpl <denner.souzapl@gmail.com>
2021-08-18 08:31:59 -0300

    registrando arquivos

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,28 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    textArray = txt_importer(path_file)
+    resultObj = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(textArray),
+        "linhas_do_arquivo": textArray,
+    }
+    sys.stdout.write(f"{resultObj}")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        removedItem = instance.dequeue()["nome_do_arquivo"]
+        sys.stdout.write(f"Arquivo {removedItem} removido com sucesso\n")
+    except IndexError:
+        sys.stdout.write("Não há elementos\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file_data = instance.search(position)
+        sys.stdout.write(f"{file_data}")
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
