0a87fe07278929876f8178776a51c7d0e3310ecd
Raphael Caputo <faelelf2@gmail.com>
2021-08-18 19:31:59 -0300

    Finalizando, os requisitos, e meu curso que tanto amei! Obriado por tudo trybe <3

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,35 @@
+def search_words_and_exists(word, instance, byWord=False):
+    result = []
+    for i in range(len(instance)):
+        file_searched = instance.search(i)
+        result.append(
+            {
+                "palavra": word,
+                "arquivo": file_searched["nome_do_arquivo"],
+                "ocorrencias": [],
+            }
+        )
+        lines = file_searched["linhas_do_arquivo"]
+        occurrence = result[0]["ocorrencias"]
+
+    for i, line in enumerate(lines):
+        if byWord:
+            if word.upper() in line.upper():
+                occurrence.append({"linha": i + 1, "conteudo": line})
+        else:
+            if word in line:
+                occurrence.append({"linha": i + 1})
+
+    if not len(occurrence):
+        return []
+    return result
+
+
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    return search_words_and_exists(word, instance)
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    return search_words_and_exists(word, instance, True)
