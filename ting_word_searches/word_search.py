cb76155d34746042577fdbdf601254f6b455c159
Fernando Antunes <fernando.antunes1@gmail.com>
2021-08-18 17:46:53 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,9 +1,6 @@
-# from ting_file_management.queue import Queue
-# from ting_file_management.file_process import process
-# import re
-
-
 def exists_word(word, instance):
+    word_original = word
+    word = word.lower()
     result = list()
     ocorrencias = list()
     files = instance.get()
@@ -13,28 +10,45 @@ def exists_word(word, instance):
         conta_linhas = 0
         for linha in linhas:
             linha = linha.replace(".", "")
-            # print("------\n")
-            # print(linha)
+            linha = linha.lower()
             palavras = linha.split(" ")
             conta_linhas += 1
             if word in palavras:
                 ocorrencias.append({"linha": conta_linhas})
-        result.append({
-            "palavra": word,
-            "arquivo": arquivo,
-            "ocorrencias": ocorrencias
-        })
+        result.append(
+            {
+                "palavra": word_original,
+                "arquivo": arquivo,
+                "ocorrencias": ocorrencias,
+            }
+        )
         if len(ocorrencias) == 0:
             return []
     return result
-    # TESTAR **********
 
 
 def search_by_word(word, instance):
-    pass
-
-
-# project = Queue()
-# process("statics/nome_pedro.txt", project)
-# word = exists_word("Pedro", project)
-# print(word)
+    word = word.lower()
+    result = list()
+    ocorrencias = list()
+    files = instance.get()
+    for file in files:
+        linhas = file["linhas_do_arquivo"]
+        arquivo = file["nome_do_arquivo"]
+        conta_linhas = 0
+        for linha in linhas:
+            linha_original = linha
+            linha = linha.replace(".", "")
+            linha = linha.lower()
+            palavras = linha.split(" ")
+            conta_linhas += 1
+            if word in palavras:
+                ocorrencias.append(
+                    {"linha": conta_linhas, "conteudo": linha_original}
+                )
+        result.append(
+            {"palavra": word, "arquivo": arquivo, "ocorrencias": ocorrencias}
+        )
+        if len(ocorrencias) == 0:
+            return []
+    return result
