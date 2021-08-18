06d445072921fd1aa4ea4df98e129b04d74fc795
Lorena Góes <goesloren@gmail.com>
2021-08-18 15:44:35 -0300

    challenge 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            sys.stderr.write("Formato inválido\n")
+        with open(path_file) as txt_file:
+            content = txt_file.read().split("\n")
+            text = list(row for row in content)
+            return text
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
