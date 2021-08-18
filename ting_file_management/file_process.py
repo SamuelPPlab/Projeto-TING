5bf0b1953ef8226e810fee16166fc0c8bed3f418
CleberFontinele <cleberfontinele2387@gmail.com>
2021-08-18 16:15:14 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    txt = txt_importer(path_file)
+    qtd_lines = len(txt)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": qtd_lines,
+        "linhas_do_arquivo": txt,
+    }
+
+    sys.stdout.write(f"{result}")
 
 
 def remove(instance):
