3423ffae0a59405304b85c4a49e589f9944d0fb2
Lorena Góes <goesloren@gmail.com>
2021-08-18 15:07:45 -0300

    challenge 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
+        self.INITIAL_INDEX = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.queue:
+            return self.queue.pop(self.INITIAL_INDEX)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < self.INITIAL_INDEX or index > len(self) - 1:
+            raise IndexError()
+        return self.queue[index]
