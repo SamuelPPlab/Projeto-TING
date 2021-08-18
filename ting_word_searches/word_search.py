749b4eaa65346727b1c4400bb34a83ea82db6ef9
dennerpl <denner.souzapl@gmail.com>
2021-08-18 09:01:54 -0300

    subindo bugs, preciso arrumar

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    resultList = []
+    for indexFile in range(len(instance)):
+        fileInPosition = instance.search(indexFile)
+        searchResult = {
+            "palavra": word,
+            "arquivo": fileInPosition,
+            "ocorrencias": [],
+        }
+
+        for indexLine, line in range(len(fileInPosition["linhas_do_arquivo"])):
+            if word.lower() in line.lower():
+                searchResult["ocorrencias"].append({"linha": indexLine + 1})
+            if len(searchResult["ocorrencias"]) > 0:
+                resultList.append(searchResult)
+    return resultList
 
 
 def search_by_word(word, instance):
