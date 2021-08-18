a5cbbe4c61d937a9bc011e10612fd4f128d849cc
Thays Costa <thayscosta3@gmail.com>
2021-08-18 09:48:06 -0300

    3rd Req.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,29 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    """
+        - Se nome de arquivo já existe em queue, ignora
+        - Importa dados do arquivo com função txt_importer
+        - Conta quantidade de linhas
+        - Adiciona um objeto à fila com:
+            -"nome_do_arquivo", "qtd_linhas" e "linhas_do_arquivo"
+        - Saída via stdout(sys) para cada adição válida
+    """
+
+    for index in range(len(instance)):
+        if path_file == instance.search(index)["nome_do_arquivo"]:
+            return None
+    file = txt_importer(path_file)
+    lenght = len(file)
+    file_content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": lenght,
+        "linhas_do_arquivo": file,
+    }
+    instance.enqueue(file_content)
+    sys.stdout.write(f"{file_content}")
 
 
 def remove(instance):
