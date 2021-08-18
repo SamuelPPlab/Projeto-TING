2af7421ae3f56420beb207c073a626c0674ff467
renatomak <renato.mark.silva@gmail.com>
2021-08-17 22:07:01 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,21 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if '.txt' not in path_file:
+            sys.stderr.write('Formato inválido\n')
+            return
+        content = open(path_file, "r")
+        result = content.read().splitlines()
+
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    else:
+        content.close()
+        return result
+
+
+# if __name__ == "__main__":
+#     arquivo = txt_importer("statics/arquivo_teste33.txt")
+#     print(arquivo)
