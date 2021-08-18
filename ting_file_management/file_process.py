10e208badbec36b235be1c2f444fb6c1a2b3e272
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2021-08-18 18:59:39 -0300

    req 2 e 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,16 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação."""
+    file = txt_importer(path_file)
+    qtd_lines = len(file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": qtd_lines,
+        "linhas_do_arquivo": file,
+    }
+
+    sys.stdout.write(f"{result}")
 
 
 def remove(instance):
