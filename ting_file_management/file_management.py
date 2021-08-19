8e60d1a25d194cbfa5bc98c83c3bb3fd57234460
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-08-19 20:30:59 -0300

    requirement 2 - with bug

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.split(".")[1] != "xml":
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file, "r") as file:
+            file.read().splitlines()  # @vanessaara
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
