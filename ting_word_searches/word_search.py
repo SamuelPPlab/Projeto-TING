b9de5b2d806fc666fafd947370940add40c5f62f
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2021-08-19 15:06:59 -0300

    reqs 6 e 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,39 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        count = []
+        for value in file["linhas_do_arquivo"]:
+            if word.lower() in value.lower():
+                count.append(
+                  {"linha": file["linhas_do_arquivo"].index(value) + 1})
+        if len(count) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": count,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        count = []
+        for value in file["linhas_do_arquivo"]:
+            if word.lower() in value.lower():
+                count.append(
+                  {"linha": file["linhas_do_arquivo"].index(value) + 1,
+                   "conteudo": value})
+        if len(count) > 0:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": count,
+                }
+            )
+    return result
