e3f2b677d15d838f50563cebcb60eada25d7f5af
Lfocosi <luccafocosi@gmail.com>
2021-08-19 15:34:48 -0300

    primeiro e ultimo commit da trybe(se não der erro)

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    """verificando se o final do arquivo é .txt"""
+    if path_file[-4:] != ".txt":
+        sys.stderr.write("Formato inválido\n")
+    try:
+        """retornando a estrutura com as linhas do arquivo"""
+        with open(path_file) as file:
+            return [line.replace("\n", "") for line in file]
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,33 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    try:
+        instance.index(path_file)
+    except ValueError:
+        instance.enqueue(path_file)
+
+    return print(
+        f"'nome_do_arquivo': '{path_file}'\n",
+        f"'qtd_linhas': {len(txt_importer(path_file))}'\n",
+        f"'linhas_do_arquivo': {txt_importer(path_file)}'",
+        file=sys.stdout,
+    )
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        return print(
+            f"Arquivo {instance.dequeue()} removido com sucesso\n",
+            file=sys.stdout,
+        )
+    except IndexError:
+        return print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        instance.search(position)
+    except IndexError:
+        return sys.stderr.write("Posição inválida")
