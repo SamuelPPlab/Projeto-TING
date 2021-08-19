e02e7447927c03451eb0165d42693f84cd31b9ef
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-08-19 19:16:55 -0300

    requirement 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,25 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue_list = list()
+        self.__length = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue_list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue_list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self) > 0:
+            returned_value = self.queue_list[0]
+            self.queue_list = self.queue_list[1:]
+        return returned_value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        value_returned = ""
+        for item_index, item in enumerate(self.queue_list):
+            if item_index == index:
+                value_returned = item
+        if index < 0 or value_returned == "":
+            raise IndexError
+        return value_returned
