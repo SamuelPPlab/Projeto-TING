2aec51bf6b68616456a045edca43176d6480b9d6
danilocouto1 <dansantosalfa@gmail.com>
2021-08-18 21:18:14 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+    controler_duplicate = False
+
+    for n in range(len(instance)):
+        if instance.search(n)["nome_do_arquivo"] == result["nome_do_arquivo"]:
+            controler_duplicate = True
+            break
+
+    if not controler_duplicate:
+        instance.enqueue(result)
+
+    sys.stdout.write(f'{result}\n')
 
 
 def remove(instance):
