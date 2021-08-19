7d7c8ca47e656543a55345f74247c6af4911db5e
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-08-19 14:38:24 -0300

    req 02 - 19/08/2021

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith("txt"):
+            with open(path_file, mode="r") as file:
+                content = file.read()
+                return content.split("\n")
+        else:
+            print("Formato inválido", file=sys.stderr)
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+
+if __name__ == "__main__":
+    print(txt_importer("statics/arquivo_nao_existe.txt"))
