d95bb9381250e5455ccecc49d7b1eca794ad7c79
CarolSi-hub <a.carolg1@gmail.com>
2021-08-19 22:44:52 -0300

    req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,17 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.files = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.files)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.files.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.files.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+        return self.files[index]
