e49a29850b1e8b05dc408daac6502a6bdd56f668
Melissa Gomes <mel.ge171201@gmail.com>
2021-08-20 16:10:11 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise TypeError()
+        with open(path_file, mode="r") as file:
+            linhasLidas = file.read().split("\n")
+            return linhasLidas
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    except TypeError:
+        print(f"Formato inválido", file=sys.stderr)
