647f27a6685923ff95fb7453afaf86a837c90d6f
Filipi Firmino <contatoic3web@gmail.com>
2021-08-20 11:52:12 -0300

    challenge 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if len(instance) < 1:
+        imported = txt_importer(path_file)
+        response = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(imported),
+            "linhas_do_arquivo": imported
+        }
+        sys.stdout.write(str(response))
+        instance.enqueue(response)
 
 
 def remove(instance):
