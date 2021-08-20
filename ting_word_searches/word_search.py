6fe4ff07f0537eb97805cf2374717e9802f54b6a
danilocouto1 <dansantosalfa@gmail.com>
2021-08-20 10:30:14 -0300

    req existe palavras

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    resultados = []
+
+    for index in range(len(instance)):
+        event = []
+        file = instance.search(index)
+        lines = file['linhas_do_arquivo']
+        counter = 0
+        for line in lines:
+            counter += 1
+            if word in line:
+                event.append({'linha': counter})
+
+        if len(event) > 0:
+            details = {}
+            details['palavra'] = word
+            details['arquivo'] = file['nome_do_arquivo']
+            details['ocorrencia'] = event
+            resultados.append(details)
+
+    return resultados
 
 
 def search_by_word(word, instance):
