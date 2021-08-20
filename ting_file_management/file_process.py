dca9b5f14bc2990d8f8fe76d297190b2d368f7c7
Vitor Rodrigues <vitor_rc@outlook.com>
2021-08-19 21:56:17 -0300

    Isso e tudo pessoal

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,40 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def generate_report(path_file):
+    file = txt_importer(path_file)
+    file_report = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+    print(file_report)
+
+
+def load_queue(instance):
+    return [instance.search(index) for index in range(len(instance))]
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    queue = load_queue(instance)
+
+    if path_file not in queue:
+        instance.enqueue(path_file)
+        generate_report(path_file)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) > 0:
+        removed_path = instance.dequeue()
+        print(f"Arquivo {removed_path} removido com sucesso")
+    else:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if len(instance) <= position:
+        print('Posição inválida', file=sys.stderr)
+    else:
+        path = instance.search(position)
+        generate_report(path)
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,36 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.file_process import load_queue
+
+
+def analyze_line(line, index, search=False):
+    result = {"linha": index + 1}
+    if search:
+        result["conteudo"] = line
+    return result
+
+
+def exists_word(word, instance, search=False):
+    occurrences = []
+    for path_file in load_queue(instance):
+        analyze = {}
+        analyze["palavra"] = word
+        analyze["arquivo"] = path_file
+
+        file = txt_importer(path_file)
+        occurrences_lines = [
+            analyze_line(line, index, search)
+            for index, line in enumerate(file)
+            if word.lower() in line.lower()
+        ]
+
+        analyze["ocorrencias"] = occurrences_lines
+
+        if len(analyze["ocorrencias"]) > 0:
+            print(analyze)
+            occurrences.append(analyze)
+
+    return occurrences
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return exists_word(word, instance, True)
