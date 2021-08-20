d8ba6ddf3bdd416fe2bdc6b20ec26977a984d9af
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-08-20 15:47:29 -0300

    requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if (
+            path_file == instance.search(index)["nome_do_arquivo"]
+        ):  # if @MoisesSantana
+            return None
+
+    file_text = txt_importer(path_file)
+    length = len(file_text)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": length,
+        "linhas_do_arquivo": file_text,
+    }
+    instance.enqueue(data)
+    sys.stdout.write(f"{data}")
 
 
 def remove(instance):
