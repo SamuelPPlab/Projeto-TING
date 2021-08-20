5b9be4488bde59e80e817f03207f7665fcad4cea
Filipi Firmino <contatoic3web@gmail.com>
2021-08-20 15:05:04 -0300

    Challenge 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    occurrence = {
+        "palavra": word,
+        "arquivo": [],
+        "ocorrencias": []
+    }
 
+    for item in range(len(instance)):
+        word_exist = instance.search(item)
+        occurrence["arquivo"] = word_exist["nome_do_arquivo"]
+        # print(occurrence)
+        for index, line in enumerate(word_exist["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                occurrence["ocorrencias"].append({"linha": index + 1})
+            if len(occurrence["ocorrencias"]):
+                result.append(occurrence)
+    return result
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
