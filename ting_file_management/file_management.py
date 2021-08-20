511a1c7e2abe1aed589ae909e618860f120b8e99
IsabelaCardoso <pcardoso.isabela@gmail.com>
2021-08-20 14:42:21 -0300

    requirement 2 - edited

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,10 +2,15 @@ import sys
 
 
 def txt_importer(path_file):
-    if path_file.split(".")[1] != "xml":
-        sys.stderr.write("Formato inválido\n")
-    try:
-        with open(path_file, "r") as file:
-            file.read().splitlines()  # @vanessaara
-    except FileNotFoundError:
-        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    if ".txt" in path_file:  # @MoisesSantana
+        try:
+            with open(path_file) as file:
+                result = []
+                for row in file:
+                    result.append(row.strip())
+
+                return result
+        except FileNotFoundError:
+            return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    return sys.stderr.write("Formato inválido\n")
