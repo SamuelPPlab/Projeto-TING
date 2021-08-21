cd79a396d5debfe060a19ff78a29e130362d0f55
julioKauer <juliokauer@gmail.com>
2021-08-21 17:26:04 -0300

    Requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,18 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue) 
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index <= len(self._queue) - 1 and index >= 0:
+            return self._queue[index]
+        else:
+            raise IndexError("IndexError")
