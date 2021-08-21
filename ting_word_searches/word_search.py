da7a39d2673d258d8186d766a8f26842e3de1cf6
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-08-21 16:37:45 -0300

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -11,7 +11,7 @@ def exists_word(word, instance):
                     "ocorrencias": []
         }
         for line in content:
-            if word in line:
+            if word.lower() in line.lower():
                 exists_in_file["ocorrencias"].append({"linha": index + 1})
         if len(exists_in_file["ocorrencias"]):
             exists.append(exists_in_file)
@@ -20,4 +20,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    index = 0
+    exists = []
+
+    while index < instance.__len__():
+        file = instance.search(index)
+        content = file["linhas_do_arquivo"]
+        exists_in_file = {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": []
+        }
+        for line in content:
+            if word.lower() in line.lower():
+                exists_in_file["ocorrencias"].append({
+                    "linha": index + 1,
+                    "conteudo": line
+                })
+        if len(exists_in_file["ocorrencias"]):
+            exists.append(exists_in_file)
+        index += 1
+    return exists
