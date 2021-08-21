968205bc949f51223e20ecb6462afacc99c0a7af
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-21 16:38:39 -0300

    como sempre tem que ter erro do lint

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,19 +1,21 @@
 class Queue:
     FIRST_ELEMENT = 0
+
     def __init__(self):
         self._data = []
 
     def __len__(self):
         return len(self._data)
-    
+
     def enqueue(self, value):
         self._data.append(value)
+
     def dequeue(self):
         if (self._data):
             return self._data.pop(self.FIRST_ELEMENT)
         return None
 
     def search(self, index):
-        if (index <  0 or self.__len__() == 0):
+        if (index < 0 or self.__len__() == 0):
             raise IndexError
         return self._data[index]
