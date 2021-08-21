0784eb920e037168ad2d20294a1dfc44afa22da5
Israel Menon <zezomenon@gmail.com>
2021-08-21 20:32:32 -0300

    Requisito 6

--- gamby.py
+++ gamby.py
@@ -1,7 +1,9 @@
 from ting_file_management.file_process import process
 from ting_file_management.queue import Queue
+from ting_word_searches.word_search import exists_word
 
 
 if __name__ == "__main__":
     fila = Queue()
-    print(process("statics/arquivo_teste.txt", fila))
+    process("statics/nome_pedro.txt", fila)
+    print(exists_word("Ratinho", fila))
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,32 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for item in range(0, instance.__len__()):
+        line = search_line(word, instance, item)
+
+    if line == []:
+        return []
+
+    result = [
+        {
+            "palavra": word,
+            "arquivo": instance.search(item)["nome_do_arquivo"],
+            "ocorrencias": [{"linha": line}],
+        }
+    ]
+    return result
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+
+def search_line(word, instance, item):
+    lists = []
+    line = 0
+    for index in instance.search(item)["linhas_do_arquivo"]:
+        result_in_lowercase = index.lower().find(word.lower())
+        if result_in_lowercase >= 0:
+            line += 1
+            lists.append(result_in_lowercase)
+        else:
+            return lists
+    return line
