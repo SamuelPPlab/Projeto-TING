3b790bd7b3d8f888493f526b7b9e21e2b045b0c8
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-08-21 17:47:54 -0300

    Feat: adds requirement three

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,16 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    process_file = {}
+    file = txt_importer(path_file)
+    process_file['nome_do_arquivo'] = path_file
+    process_file['qtd_linhas'] = len(file)
+    process_file['linhas_do_arquivo'] = file
+    instance.enqueue(process_file)
+    sys.stdout.write(f"{process_file}\n")
+    return process_file
 
 
 def remove(instance):
