007f1de2ee74ce04a0b1645ae412af0d4c824cae
Pessoal <pessoal@nb-004842.cwinet.local>
2021-08-21 17:23:38 -0300

    foi dois

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,10 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file[-4:] != '.txt':
+        sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file) as news_file:
+            return news_file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
