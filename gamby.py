df08d490c35618731bbcd83606a0c975d102d8f6
Israel Menon <zezomenon@gmail.com>
2021-08-21 19:11:17 -0300

    Requisito 3, requisito 4, requisito 5

--- gamby.py
+++ gamby.py
@@ -1,5 +1,7 @@
-from ting_file_management.file_management import txt_importer
+from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
 
 
 if __name__ == "__main__":
-    print(txt_importer("statics/arquivo_teste.txt"))
+    fila = Queue()
+    print(process("statics/arquivo_teste.txt", fila))
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    for item in range(0, instance.__len__()):
+        if instance.search(item)["nome_do_arquivo"] == path_file:
+            return None
+
+    instance.enqueue(result)
+
+    print(result)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    item = instance.dequeue()
+    if not item:
+        return print("Não há elementos")
+    return print(f"Arquivo {item['nome_do_arquivo']} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        item = instance.search(position)
+        return print(item)
+    except IndexError:
+        sys.stderr.write("Posição inválida")
+
+
+# Source: Anderson Alves / Emerson Junqueira
