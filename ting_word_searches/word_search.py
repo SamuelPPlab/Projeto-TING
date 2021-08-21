14d15a2a893e09a74ddef6a83768fb7180372bfd
Lenodeoliveira <lenodeoliveira@gmail.com>
2021-08-21 20:38:39 -0300

    feat: checking if the words exist

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,16 @@
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = instance.get_data()
+
+    for item in files:
+        if not any(word in w for w in item['linhas_do_arquivo']):
+            return []
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    files = instance.get_data()
+
+    for item in files:
+        if not any(word in w for w in item['linhas_do_arquivo']):
+            return []
