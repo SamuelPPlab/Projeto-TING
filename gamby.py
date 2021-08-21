225ac7880fbb3ca171843449fdfaba96268e33d3
Israel Menon <zezomenon@gmail.com>
2021-08-21 18:25:22 -0300

    Requisito 2

---
+++ gamby.py
@@ -0,0 +1,5 @@
+from ting_file_management.file_management import txt_importer
+
+
+if __name__ == "__main__":
+    print(txt_importer("statics/arquivo_teste.txt"))
--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file, "rt", newline="\n") as file_in:
+                lines = []
+                for line in file_in:
+                    lines.append(line.strip())
+                return lines
+        else:
+            return sys.stderr.write("Formato inválido\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
+# https://www.codegrepper.com/code-examples/python/reading+a+text+file+line+by+line+in+python
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
