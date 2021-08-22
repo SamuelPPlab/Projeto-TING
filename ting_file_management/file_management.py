5d3b82dd72707e02d8c961fb48c74debe1984856
Bruno <bruno_grb@hotmail.com>
2021-08-22 16:43:57 -0300

    requisito 2 pronto

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file, mode="r") as file:
+            return file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
