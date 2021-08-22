9f0ed5d80a2a26ebd0c117c771c2e4f564d2596d
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-08-22 16:09:22 -0300

    requisito 06-v1

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    my_dict = {
+        "palavra": word,
+        "arquivo": "",
+        "ocorrencias": [],
+    }
+
+    for i in range(len(instance)):
+        file = instance.search(i)
+        print(f"********{file}")
+        my_dict["arquivo"] = file["nome_do_arquivo"]
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word in line:
+                my_dict["ocorrencias"].append({"linha": index + 1})
+        if len(my_dict["ocorrencias"]) == 0:
+            return []
+    return [my_dict]
 
 
 def search_by_word(word, instance):
