a3861f686ec4313228329c1a4a75460151631e3b
Strongreen <strongreen@live.com>
2021-08-22 20:35:21 -0300

    requisito 3 - rumo ao hexa

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def build(path_file):
+    content = txt_importer(path_file)
+    return {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content
+    }
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    try:
+        if instance.__len__():
+            raise ValueError()
+        else:
+            content_file = build(path_file)
+            sys.stdout.write(f"{content_file}\n")
+            instance.enqueue(content_file)
+    except ValueError:
+        print('Posição inválida', file=sys.stderr)
 
 
 def remove(instance):
