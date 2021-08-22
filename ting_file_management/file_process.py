513dc9ec69133d65ba026cbc0e11976b5ba585c0
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-08-22 14:20:12 -0300

    requisito 03-v1

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá suaaaaaaa implementação"""
+    file = txt_importer(path_file)
+    my_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+
+    sys.stdout.write(f"{my_dict}")
+    instance.enqueue(my_dict)
 
 
 def remove(instance):
