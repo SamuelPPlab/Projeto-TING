8cbdda8771997f4fccabd53a666ac76f6a04152e
Bruno <bruno_grb@hotmail.com>
2021-08-22 20:07:38 -0300

    iniciando requisito 6 e 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,30 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words = []
+    length = instance.__len__()
+
+    for i in range(length):
+        position = instance.search(i)
+        result = {
+            "palavra": word,
+            "arquivo": position,
+            "ocorrencias": [],
+        }
+        for index, line in enumerate(position["linhas_do_arquivo"]):
+            if len(result["ocorrencias"]) > 0:
+                words.append(result)
+
+            if word.lower() in line.lower():
+                result["ocorrencias"].append({"linha": index + 1})
+    return words
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = exists_word(word, instance)
+    if result:
+        for index, line in enumerate(txt_importer(instance.search(0))):
+            if index == result[0]["ocorrencias"][index]["linha"] - 1:
+                result[0]["ocorrencias"][index]["conteudo"] = line
+    return result
