e3b324a48ea35ad9d09cdd411dfd928c2671d9fe
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-22 17:11:00 -0300

    Requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_found = [{
+        'palavra': word,
+        'arquivo': '',
+        'ocorrencias': [],
+    }]
+    for instance_len in range(len(instance)):
+        file = instance.search(instance_len)
+        word_found[0]['arquivo'] = file['nome_do_arquivo']
+        ocurrencies_found = word_found[0]['ocorrencias']
+
+        for index, line in enumerate(file['linhas_do_arquivo']):
+            if word in line:
+                ocurrencies_found.append({'linha': index + 1})
+        if len(ocurrencies_found) > 0:
+            return word_found
+        else:
+            return ocurrencies_found
 
 
 def search_by_word(word, instance):
