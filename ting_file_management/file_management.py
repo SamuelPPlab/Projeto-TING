7029536cbdcc5176594459fbbb5add94fc7c3f3b
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-08-22 12:57:08 -0300

    requisito 02-v2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,17 +1,13 @@
 import sys
 
 
-def get_file_text(path_file):
-    try:
-        with open(path_file) as fd:
-            lines = fd.readlines()
-            return lines
-    except FileNotFoundError:
-        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
-
-
 def txt_importer(path_file):
     if not (path_file.endswith('.txt')):
         return sys.stderr.write("Formato inválido\n")
 
-    get_file_text(path_file)
+    try:
+        with open(path_file) as fd:
+            lines = fd.readlines()
+            return [line.rstrip() for line in lines]
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
