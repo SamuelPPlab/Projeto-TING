575913eec3f1e463c82f21c8aca4a5a04eb17edf
alvarovascon <alvarovascon2@gmail.com>
2021-08-22 14:19:16 -0300

    I can do this all day

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,4 @@
-def get_ocurrences(data, word):
+def get_ocurrences(data, word, with_content):
     element = {
         "palavra": word,
         "arquivo": data["nome_do_arquivo"],
@@ -7,17 +7,24 @@ def get_ocurrences(data, word):
     for index, line in enumerate(data["linhas_do_arquivo"]):
         if word.lower() in line.lower():
             element["ocorrencias"].append({"linha": index + 1})
+            if with_content:
+                element["ocorrencias"][index]["conteudo"] = line
     return element
 
 
 def exists_word(word, instance):
     result = list()
     for sample in instance._queue:
-        element = get_ocurrences(sample, word)
+        element = get_ocurrences(sample, word, 0)
         if len(element["ocorrencias"]) > 0:
             result.append(element)
     return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    for sample in instance._queue:
+        element = get_ocurrences(sample, word, 1)
+        if len(element["ocorrencias"]) > 0:
+            result.append(element)
+    return result
