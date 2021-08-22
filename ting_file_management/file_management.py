4a07a61ea55cd9edc77a842247795652708a654c
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-22 15:21:05 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido\n')
+    try:
+        file = open(path_file, 'r')
+        file_list = list()
+        for row in file:
+            file_list.append(row.rstrip('\n'))
+        return file_list
+    except IOError:
+        error_message = f'Arquivo {path_file} não encontrado\n'
+        sys.stderr.write(error_message)
