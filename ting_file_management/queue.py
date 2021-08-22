6fa87364a418cfb83664162a54df0e6af0ac45e9
Bruno <bruno_grb@hotmail.com>
2021-08-22 16:39:24 -0300

    requisito 1 pronto

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,28 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
+
+    def remove_first(self):
+        value_to_be_removed = self.head_value
+        if value_to_be_removed:
+            self.head_value = self.head_value.next
+            value_to_be_removed.next = None
+            self.__length -= 1
+        return value_to_be_removed
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self._data:
+            return self._data.pop(0)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        length = self.__len__()
+        if length == 0 or index < 0:
+            raise IndexError()
+        return self._data[index]
