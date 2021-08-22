8ec245915d768b8372261828a9699a7046a5a32a
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-21 23:36:42 -0300

    feat: first requirements

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if (len(self.queue) - 1) > index < 0:
+            raise IndexError
+        return self.queue[index]
+
+
+""" Aula 38.1 course """
