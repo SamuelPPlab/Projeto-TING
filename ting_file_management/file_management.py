d6dd9d0fa048601509b0d4c4e73c711373245b73
Strongreen <strongreen@live.com>
2021-08-22 18:58:06 -0300

    requisito 2 - corrigindo complexidade - segunda tentativa

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,19 +1,12 @@
 import sys
 import os.path
 
-
-def readTxt(path_file):
-    with open(path_file, 'r',) as file:
-        return file.read().splitlines()
-
-
 def txt_importer(path_file):
     try:
         if not path_file.endswith('.txt'):
             raise ValueError()
-        if not (os.path.exists(path_file)):
-            raise FileNotFoundError()
-        return readTxt(path_file)
+        with open(path_file, 'r',) as file:
+            return file.read().splitlines()
     except FileNotFoundError:
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
     except ValueError:
