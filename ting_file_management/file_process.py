1acaf0bac44a17b469c342ec565b057ab3e399c0
Carol Bezerra <carol.arbe@gmail.com>
2021-08-22 15:47:20 -0300

    acrescentando ao requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,17 +3,18 @@ import sys
 
 
 def process(path_file, instance):
-    arquivo = txt_importer(path_file)  # usando a função que fiz no req.2
-    conta_linhas = len(arquivo)  # cada linha é 1 item
-    dict_retorno = {
-        "nome_do_arquivo": path_file,
-        "qtd_linhas": conta_linhas,
-        "linhas_do_arquivo": arquivo
-        }  # saída esperada
+    if len(instance) < 1:  # pra saber que ta iniciando a lista, referência: Vanessa Bidinotto
+        arquivo = txt_importer(path_file)  # usando a função que fiz no req.2
+        conta_linhas = len(arquivo)  # cada linha é 1 item
+        dict_retorno = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": conta_linhas,
+            "linhas_do_arquivo": arquivo
+            }  # saída esperada
 
-    instance.enqueue(dict_retorno)  # adiciona na lista
-    sys.stdout.write(f"{dict_retorno}")
-    # a saída tem que ter esse out, referência: Luíse Rios
+        instance.enqueue(dict_retorno)  # adiciona na lista
+        sys.stdout.write(f"{dict_retorno}")
+        # a saída tem que ter esse out, referência: Luíse Rios
 
 
 def remove(instance):
