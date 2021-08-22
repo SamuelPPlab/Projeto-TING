7c4b4e5972811434a510d4f426cb8cbf611c124d
Carol Bezerra <carol.arbe@gmail.com>
2021-08-22 10:46:58 -0300

    requisito 2 passando no local

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys  # Referência: Luíse Rios
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        arquivo = open(path_file, 'r')
+        texto = [linha for linha in arquivo.read().split("\n")]
+        # essa ideia do read().split() foi da Luíse Rios
+        arquivo.close()
+        return texto
+    except FileNotFoundError:
+        sys.stderr.write(
+            "Arquivo statics/arquivo_nao_existe.txt não encontrado\n"
+            )
