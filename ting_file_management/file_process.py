0920b0acafb7c9f380fdc1188f86f6ba25b12f3d
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-08-22 14:51:03 -0300

    requisito 03-v2

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,6 +4,11 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     file = txt_importer(path_file)
+
+    for i in range(len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == path_file:
+            return None
+
     my_dict = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(file),
