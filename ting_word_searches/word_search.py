22bebd037b531cc95d2cf6a2bf2c8d1df667fcae
Melissa Gomes <mel.ge171201@gmail.com>
2021-08-22 19:22:04 -0300

    test

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+    for i in range(len(instance)):
+        posicao = instance.search(i)
+        resultadoDaBusca = {
+            "palavra": word,
+            "arquivo": posicao,
+            "ocorrencias": [],
+        }
+
+        for posicaoDaLinha, linha in enumerate(posicao["linhas_do_arquivo"]):
+            if len(resultadoDaBusca["ocorrencias"]) > 0:
+                results.append(resultadoDaBusca)
+
+            if word.lower() in linha.lower():
+                resultadoDaBusca["ocorrencias"].append(
+                    {"linha": posicaoDaLinha + 1}
+                )
+
+    return results
 
 
 def search_by_word(word, instance):
