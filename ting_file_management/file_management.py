c3020c4572d0a3555b677896332b69836091fabc
jr3564 <rodolfojr94@gmail.com>
2021-08-22 16:15:57 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    print(path_file[-3:])
+    if path_file[-3:] != "txt":
+        message = "Formato inválido"
+        print(message, file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as txt_file:
+            return [line.rstrip("\n") for line in txt_file.readlines()]
+    except FileNotFoundError:
+        message = "Arquivo statics/arquivo_nao_existe.txt não encontrado"
+        print(message, file=sys.stderr)
