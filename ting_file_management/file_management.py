11c5d0e86c3c33f558db8fcefa246fbd816a6629
Carol Bezerra <carol.arbe@gmail.com>
2021-08-22 11:08:05 -0300

    requisito 3 passando no local

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,7 +2,6 @@ import sys  # Referência: Luíse Rios
 
 
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
     if not path_file.endswith(".txt"):
         sys.stderr.write("Formato inválido\n")
 
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    arquivo = txt_importer(path_file)  # usando afunção que fiz no req.2
+    conta_linhas = len(arquivo)  # cada linha é 1 item
+    dict_retorno = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": conta_linhas,
+        "linhas_do_arquivo": arquivo
+        }  # saída esperada
+
+    instance.enqueue(dict_retorno)  # adiciona na lista
+    sys.stdout.write(f"{dict_retorno}")
+    # a saída tem que ter esse out, referência: Luíse Rios
 
 
 def remove(instance):
@@ -8,3 +23,4 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
