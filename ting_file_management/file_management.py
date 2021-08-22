b645770f5e5c5115584c772f451d19393a5761d3
Strongreen <strongreen@live.com>
2021-08-22 18:41:19 -0300

    requisito 2 e dessa vez lembrei do flake8

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
+import sys
+import os.path
+
+
+def readTxt(path_file):
+    with open(path_file, 'r',) as file:
+        return file.read().splitlines()
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith('.txt'):
+            if os.path.exists(path_file):
+                return readTxt(path_file)
+            else:
+                raise FileNotFoundError()
+        else:
+            raise ValueError()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    except ValueError:
+        print(f"Formato inválido", file=sys.stderr)
