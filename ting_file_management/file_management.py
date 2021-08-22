6121876f9092d36c0b7378da6bdb473bd275bf05
Lucas Andrade <lafdesouza2002@outlook.com>
2021-08-22 19:06:29 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+import os
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith('.txt'):
+        if not os.path.isfile(path_file):
+            return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+        with open(path_file, 'r', encoding='utf-8') as file:
+            return file.read().splitlines()
+    else:
+        return sys.stderr.write('Formato inválido\n')
+
+# txt_importer('C:/Users/lafde/sd-07-project-ting/statics/arquivo_teste.txt')
