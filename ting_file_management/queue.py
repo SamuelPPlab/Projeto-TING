75164a9e1e413039b1b56158535aea65b90b2875
Carol Bezerra <carol.arbe@gmail.com>
2021-08-22 10:07:59 -0300

    requisito 1 passando no local

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.lista = []
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.lista)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        return self.lista.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        return self.lista.pop(0)
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+
+        return self.lista[index]
