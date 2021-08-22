99153c962e581e8a0510853e3ddb3913291f8e65
danielmlduarte <danielduarte.tryber@gmail.com>
2021-08-22 16:25:19 -0300

    Requisito 3 e 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,18 +4,23 @@ from ting_file_management.file_management import txt_importer
 
 def process(path_file, instance):
     news = txt_importer(path_file)
-    if instance.search_by_value(path_file) < 1:
-        file_data = {
-            'nome_do_arquivo': path_file,
-            'qtd_linhas': len(news),
-            'linhas_do_arquivo': news,
-        }
-        instance.enqueue(file_data)
-        sys.stdout.write(str(file_data))
+    if len(instance) == 0:
+        if instance.search_by_value(path_file) < 1:
+            file_data = {
+                'nome_do_arquivo': path_file,
+                'qtd_linhas': len(news),
+                'linhas_do_arquivo': news,
+            }
+            instance.enqueue(file_data)
+            sys.stdout.write(str(file_data))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print('Não há elementos')
+    else:
+        path_file = instance.dequeue()['nome_do_arquivo']
+        sys.stdout.write(f'Arquivo {path_file} removido com sucesso\n')
 
 
 def file_metadata(instance, position):
