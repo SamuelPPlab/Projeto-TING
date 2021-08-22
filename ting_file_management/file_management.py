a8495f3244c98524b1b7693a2d1032f21f91eb93
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-08-21 22:00:33 -0300

    requisito 02-v1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
+def get_file_text(path_file):
+    try:
+        with open(path_file) as fd:
+            lines = fd.readlines()
+            return lines
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not (path_file.endswith('.txt')):
+        return sys.stderr.write("Formato inválido\n")
+
+    get_file_text(path_file)
