44d28a2a65d75ae3641768e13168188270ce741b
Igor-leao <igorleao.eng@gmail.com>
2021-08-22 09:33:34 -0300

    requisito 6 implementaçao exists_word requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data = instance.get_data()
+
+    output = list()
+
+    for file in data:
+        file_data = txt_importer(file)
+
+        count = list()
+
+        for index, line in enumerate(file_data):
+            if word in line:
+                count.append({'linha': index + 1})
+
+        if len(count) > 0:
+            output.append({
+                'palavra': word,
+                'arquivo': file,
+                'ocorrencias': count
+            })
+
+    return output
 
 
 def search_by_word(word, instance):
