82a36d58bf2d253423061819ccce7c12a8eef9c8
LuizFernandesOliveira <luiz.fernandes@academico.ifrn.edu.br>
2021-08-22 17:17:44 -0300

    req6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = []
+
+    for index in range(instance.__len__()):
+        file = instance.search(index)
+        result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {
+                    "linha": (file["linhas_do_arquivo"].index(line) + 1)
+                    for line in file["linhas_do_arquivo"]
+                    if word.lower() in line.lower()
+                }
+            ],
+        }
+
+        if result["ocorrencias"] != [{}]:
+            files.append(result)
+
+    return files
 
 
 def search_by_word(word, instance):
