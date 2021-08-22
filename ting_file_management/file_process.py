0f46874008a6616a783b34975aa478e432fb1d0a
CarolSi-hub <a.carolg1@gmail.com>
2021-08-22 19:19:21 -0300

    My heart will go oooooon

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,37 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
+def find_file(path_file, instance):
+    return path_file in instance.files
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if not find_file(path_file, instance):
+        file_content = txt_importer(path_file)
+        instance.enqueue(path_file)
+        lines = list()
+        for line in file_content:
+            lines.append(line)
+        lines_count = len(lines)
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": lines_count,
+            "linhas_do_arquivo": file_content
+        }
+
+        sys.stdout.write(str(result))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        return sys.stdout.write("Não há elementos\n")
+    file = instance.dequeue()
+    sys.stdout.write(
+        f"Arquivo {file} removido com sucesso\n"
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position > instance.__len__() - 1:
+        return sys.stderr.write("Posição inválida\n")
