07afd90488ec79eff65201bafa39375f8a617b99
marcelo ivan <sagacello@hotmail.com>
2021-08-23 15:31:01 -0300

    funcao de remove e metadata

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,19 +1,29 @@
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file, instance):
-    process_dict = {}
-    txt = txt_importer(path_file)
-    process_dict['nome_do_arquivo'] = path_file
-    process_dict['qtd_linhas'] = len(txt)
-    process_dict['linhas_do_arquivo'] = txt
-    print(process_dict)
-    instance.enqueue(process_dict)
+    if path_file not in instance.my_file_names:
+        process_dict = {}
+        txt = txt_importer(path_file)
+        process_dict["nome_do_arquivo"] = path_file
+        process_dict["qtd_linhas"] = len(txt)
+        process_dict["linhas_do_arquivo"] = txt
+        print(process_dict)
+        instance.enqueue(process_dict)
 
 
 def remove(instance):
-    """"""
+    if len(instance) == 0:
+        print("Não há elementos")
+    else:
+        name = instance.my_queue[0]["nome_do_arquivo"]
+        print(f"Arquivo {name} removido com sucesso")
+        instance.dequeue()
 
 
 def file_metadata(instance, position):
-    """"""
+    try:
+        print(instance.my_queue[position])
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -4,12 +4,14 @@ import sys
 class Queue:
     def __init__(self):
         self.my_queue = []
+        self.names = set()
 
     def __len__(self):
         return len(self.my_queue)
 
     def enqueue(self, value):
         self.my_queue.append(value)
+        self.names.add(value['nome_do_arquivo'])
 
     def dequeue(self):
         return self.my_queue.pop(0)
