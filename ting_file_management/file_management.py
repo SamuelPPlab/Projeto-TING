ed4bdd61b425c8c2f852ffb5c6cb3f365ccdc848
vitorAmorims <vtamorims@gmail.com>
2021-08-23 18:41:55 -0300

    test_validar_importar_noticias_com_sucesso

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+# def verificar_arquivo():
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    resp = []
+    try:    
+        with open(path_file, "r") as f:
+            contents = f.readlines()
+            for index in contents:
+                resp.append(index.strip())
+    except FileNotFoundError:
+        return Exception(f"Arquivo {path_file} não encontrado")
+    else:
+        return resp
+
+print(txt_importer("../statics/arquivo_teste.txt"))
