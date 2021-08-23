f7db0c3c5b13f4af13fe7c125b2733c9060eb3d7
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-08-23 20:22:29 -0300

    process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,16 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
 
+def process(path_file, instance):
+    file_process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+
+    instance.enqueue(file_process)
+
+    print(file_process)
 
 def remove(instance):
     """Aqui irá sua implementação"""
