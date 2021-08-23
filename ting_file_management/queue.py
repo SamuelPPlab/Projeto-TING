0a007ffc53fd8f3ec59af1cdafe15202e53f2910
danwhat <daanwhat@gmail.com>
2021-08-23 19:23:55 -0300

    req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,33 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.lista = list()
+        self.tamanho = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.lista)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.lista.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.lista.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        retorno = None
+        if index < 0:
+            raise IndexError()
+        try:
+            retorno = self.lista[index]
+        except IndexError:
+            raise IndexError('list index out of range')
+        return retorno
+
+if __name__ == '__main__':
+    q = Queue()
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    print(q.search(0))
+    print(len(q))
+    q.dequeue()
+    print(q.search(0))
