d852950dc1055c1ab5f75b6df2bccf55f0e1417b
LAURO DE SA <itrafor@gmail.com>
2021-08-23 11:58:17 -0300

    Requisito 6.2

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,9 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    return []
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+# Referencia: arthur-massaini-project-ting (#63)
