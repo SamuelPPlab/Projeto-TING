e085d02d299376a111aefb8d0974839e997a36d4
marcelo ivan <sagacello@hotmail.com>
2021-08-23 15:58:35 -0300

    extist_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    overall = []
+    queue = instance.my_queue
+    for item in queue:
+        item_dict = {
+            'palavra': word,
+            'arquivo': item['nome_do_arquivo']
+        }
+        appears = []
+        index = 1
+        for line in item['linhas_do_arquivo']:
+            if (line.lower()).find(word.lower()) != -1:
+                line_dict = {
+                    'linha': index
+                }
+                appears.append(line_dict)
+            index += 1
+        item_dict['ocorrencias'] = appears
+        overall.append(item_dict)
+    if len(appears) == 0:
+        return []
+    else:
+        return overall
 
 
 def search_by_word(word, instance):
