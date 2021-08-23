b44b13b08af79220c88e156053d89fc8ca9d4873
danwhat <daanwhat@gmail.com>
2021-08-23 20:31:46 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+  try:
+    if path_file[-4:] != '.txt':
+      raise ValueError()
+    with open(path_file, "r" ) as f:
+        txt = f.read()
+        return txt.split("\n")
+    print(array)
+    return array
+  except FileNotFoundError:
+    print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+  except ValueError:
+    print("Formato inválido", file=sys.stderr)
+     
+
+if __name__ == '__main__':
+  txt_importer("statics/arquivo_teste.txt")
