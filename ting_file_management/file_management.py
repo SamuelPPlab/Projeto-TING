9ad6ad7797bbb5164446aed26a9d0847358dac86
Bruno Nogueira <nugobruno@gmail.com>
2021-08-23 01:32:07 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+
+    # source of file extension check:
+    # https://github.com/tryber/sd-07-project-ting/
+    # blob/rodolfo-project-ting/ting_file_management/
+    # file_management.py
+    if (path_file[-3:] != 'txt'):
+        print('Formato inválido', file=sys.stderr)
+
+    try:
+        file = open(path_file, 'r')
+        return file.read().split('\n')
+    except FileNotFoundError:
+        print(
+            'Arquivo statics/arquivo_nao_existe.txt não encontrado',
+            file=sys.stderr
+        )
