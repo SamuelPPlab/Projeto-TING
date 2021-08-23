45342f1793c51cf250eebcb28ae1022a913c134e
vitorAmorims <vtamorims@gmail.com>
2021-08-23 20:47:42 -0300

    correcao sintaxe flake8

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -2,17 +2,18 @@ import sys
 # https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
 # def verificar_arquivo():
 
+
 def txt_importer(path_file):
     resp = []
     if not path_file.endswith(".txt"):
         sys.stderr.write("Formato inválido\n")
-    try:    
+    try:
         with open(path_file, "r") as f:
             contents = f.readlines()
             for index in contents:
                 resp.append(index.strip())
     except FileNotFoundError:
-         sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
     else:
         return resp
 
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,17 +1,22 @@
 import sys
 # import os
 from ting_file_management.file_management import txt_importer
-from ting_file_management.queue import Queue
+# from ting_file_management.queue import Queue
 # teste = Queue()
+
 
 def process(path_file, instance):
     # nome_arquivo = os.path.basename(path_file)
-    tot_linhas = 0
     data = txt_importer(path_file)
     if len(instance) < 1:
-        resp = {'nome_do_arquivo':  path_file, "qtd_linhas": len(data), "linhas_do_arquivo": data}
+        resp = {
+            'nome_do_arquivo':  path_file,
+            "qtd_linhas": len(data),
+            "linhas_do_arquivo": data
+        }
         sys.stdout.write(str(resp))
-        instance.enqueue(resp)    
+        instance.enqueue(resp)
+
 
 def remove(instance):
     if len(instance) == 0:
@@ -21,7 +26,7 @@ def remove(instance):
     return sys.stdout.write(
         f"Arquivo {file_removed} removido com sucesso\n"
     )
-    
+
 
 def file_metadata(instance, position):
     if position > len(instance):
