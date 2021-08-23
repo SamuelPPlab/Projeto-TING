f2d2c8dedf2f1964becb59143094699732232e8e
marcelo ivan <sagacello@hotmail.com>
2021-08-23 15:41:56 -0300

    Agora sim

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -11,9 +11,17 @@ class Queue:
 
     def enqueue(self, value):
         self.my_queue.append(value)
-        self.my_file_names.add(value['nome_do_arquivo'])
+        print(type(value) == 'dict')
+        if type(value) is dict:
+            print(value['nome_do_arquivo'])
+            print('olaRRRRRRRRRR')
+            self.my_file_names.add(value['nome_do_arquivo'])
+        else:
+            print('erro')
 
     def dequeue(self):
+        if type(self.my_queue[0]) is dict:
+            self.my_file_names.discard(self.my_queue[0]['nome_do_arquivo'])
         return self.my_queue.pop(0)
 
     def search(self, index):
