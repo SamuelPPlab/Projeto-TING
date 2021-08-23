de986e78699bf1c797b23cc8fc2dab8047a11e20
Melissa Gomes <mel.ge171201@gmail.com>
2021-08-23 17:59:45 -0300

    me passa

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,23 +2,23 @@ def exists_word(word, instance):
     results = []
     for i in range(len(instance)):
         posicao = instance.search(i)
+        print("-----------------", posicao)
         resultadoDaBusca = {
             "palavra": word,
-            "arquivo": posicao,
+            "arquivo": posicao["nome_do_arquivo"],
             "ocorrencias": [],
         }
 
         for posicaoDaLinha, linha in enumerate(posicao["linhas_do_arquivo"]):
-            if len(resultadoDaBusca["ocorrencias"]) > 0:
-                results.append(resultadoDaBusca)
-
             if word.lower() in linha.lower():
                 resultadoDaBusca["ocorrencias"].append(
                     {"linha": posicaoDaLinha + 1}
                 )
+            if len(resultadoDaBusca["ocorrencias"]) > 0:
+                results.append(resultadoDaBusca)
 
     return results
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    """Return the lines with the content where the word exists"""
