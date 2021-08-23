14951dae681f6730165566cfe5256c6bbf29ea8d
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-08-23 16:55:28 -0300

    linter02

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,4 @@ def search_by_word(word, instance):
 # python3 -m pytest tests/test_word_search.py
 
 # referencias
-# https://app.betrybe.com/course/live-lectures/sd-cohort-6#aula-383-estrutura-de-dados-i-set - 39.1 33:00
+# ...
