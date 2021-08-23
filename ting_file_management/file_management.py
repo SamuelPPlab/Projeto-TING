7109642dd32b78a7be94e3c0c7c17cf14f1270a9
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-08-23 19:25:06 -0300

    file anagement

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not (path_file.endswith(".txt")):
+        return print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, mode="r") as file:
+            lines = [line.rstrip('\n') for line in file]
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado",file=sys.stderr )
+
+# https://qastack.com.br/programming/3277503/how-to-read-a-file-line-by-line-into-a-lists
