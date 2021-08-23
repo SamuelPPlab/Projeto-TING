a66e1ff456abb37695bd32a332e5e9407324da49
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-08-23 20:16:18 -0300

    requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if path_file == instance.search(i).get("nome_do_arquivo"):
+            return
+
+    lines = txt_importer(path_file)
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(file_info)
+    print(file_info)
 
 
 def remove(instance):
