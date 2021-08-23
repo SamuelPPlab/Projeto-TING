8e45b67520d47d9700ddb9ecfa6497c1600bd208
vitorAmorims <vtamorims@gmail.com>
2021-08-23 19:58:57 -0300

    test_validar_funcao_process_com_sucesso

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,19 +1,18 @@
-import os
+import sys
+# import os
 from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
-
-teste = Queue()
+# teste = Queue()
 
 def process(path_file, instance):
-    nome_arquivo = os.path.basename(path_file)
+    # nome_arquivo = os.path.basename(path_file)
     tot_linhas = 0
     data = txt_importer(path_file)
-    for index in range(len(data)):
-        tot_linhas += index
-    resp = {"nome_do_arquivo": nome_arquivo, "qtd_linhas": tot_linhas, "linhas_do_arquivo": data}
+    # for index in range(len(data)):
+    #     tot_linhas += index
+    resp = {'nome_do_arquivo':  path_file, "qtd_linhas": len(data), "linhas_do_arquivo": data}
+    sys.stdout.write(str(resp))
     instance.enqueue(resp)
-    return resp
-    
     
     # instance.enqueue()
     # return resp
@@ -25,4 +24,4 @@ def remove(instance):
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
 
-print(process("../statics/arquivo_teste.txt", teste))
+# print(process("../statics/arquivo_teste.txt", teste))
