31e9cd113690d3d5d56871a43b434bda409a29fa
marcelo ivan <sagacello@hotmail.com>
2021-08-23 13:54:17 -0300

    requisitos 1 2 e 3

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+    try:
+        arr = []
+        with open(path_file, "r") as file:
+            for line in file.readlines():
+                line = line.replace("\n", "")
+                arr.append(line)
+        return arr
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    process_dict = {}
+    txt = txt_importer(path_file)
+    process_dict['nome_do_arquivo'] = path_file
+    process_dict['qtd_linhas'] = len(txt)
+    process_dict['linhas_do_arquivo'] = txt
+    print(process_dict)
+    instance.enqueue(process_dict)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    """"""
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    """"""
