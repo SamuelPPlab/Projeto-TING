8c54195abd3790ea0e5078a6e440b2977644ddee
brendalima <brenda.c.lima@poli.ufrj.br>
2021-08-24 08:09:41 -0300

    requirement n5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -28,4 +28,7 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return instance.search(position)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
