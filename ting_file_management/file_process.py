ea049c477ad103a85f4422c5aae1fd2f9d8981ef
julioKauer <juliokauer@gmail.com>
2021-08-24 20:50:02 -0300

    Requisito 6 e 7

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,14 +3,19 @@ from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    if path_file not in instance._queue:
-        instance.enqueue(path_file)
+    filenames = [file_data["nome_do_arquivo"] for file_data in instance._queue]
 
-    print(
-        f"'nome_do_arquivo': '{path_file}'\n",
-        f"'qtd_linhas': {len(txt_importer(path_file))}'\n",
-        f"'linhas_do_arquivo': {txt_importer(path_file)}'",
-        file=sys.stdout,
+    if path_file not in filenames:
+        file_data = {}
+        file_data["nome_do_arquivo"] = path_file
+        file_data["linhas_do_arquivo"] = txt_importer(path_file)
+        file_data["qtd_linhas"] = len(file_data["linhas_do_arquivo"])
+        instance.enqueue(file_data)
+
+        print(
+            f"'nome_do_arquivo': '{path_file}'\n",
+            f"'qtd_linhas': {file_data['qtd_linhas']}\n",
+            f"'linhas_do_arquivo': {file_data['linhas_do_arquivo']}",
         )
 
 
@@ -18,8 +23,9 @@ def remove(instance):
     if instance.__len__() == 0:
         return sys.stdout.write("Não há elementos\n")
 
+    file_path = instance.dequeue()["nome_do_arquivo"]
     print(
-        f"Arquivo {instance.dequeue()} removido com sucesso\n",
+        f"Arquivo {file_path} removido com sucesso\n",
         file=sys.stdout,
     )
 
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,42 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for file_data in instance._queue:
+        occurrences = []
+        for index, line in enumerate(file_data["linhas_do_arquivo"]):
+            if re.search(word, line, re.IGNORECASE):
+                occurrences.append({"linha": index + 1})
+
+        if occurrences != []:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_data["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for file_data in instance._queue:
+        occurrences = []
+        for index, line in enumerate(file_data["linhas_do_arquivo"]):
+            if re.search(word, line, re.IGNORECASE):
+                occurrences.append({
+                    "linha": index + 1,
+                    "conteudo": line,
+                    })
+
+        if occurrences != []:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_data["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return result
