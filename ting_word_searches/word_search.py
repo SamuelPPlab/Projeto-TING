281a2d87c000319703722e889c1ebb79565c5da6
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-08-24 17:20:40 -0300

    requisito 6

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,31 @@
+from ting_file_management.file_process import process
+from ting_file_management.queue import Queue
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    current_file = instance.dequeue()
+
+    lines = current_file.get("linhas_do_arquivo")
+
+    occurrences = []
+
+    for index, line in enumerate(lines):
+        parsed_line = line.lower()
+        parsed_word = word.lower()
+
+        if parsed_word in parsed_line:
+            occurrences.append({"linha": index + 1})
+
+    if len(occurrences) == 0:
+        return []
+
+    return [
+        {
+            "palavra": word,
+            "arquivo": current_file.get("nome_do_arquivo"),
+            "ocorrencias": occurrences,
+        }
+    ]
 
 
 def search_by_word(word, instance):
