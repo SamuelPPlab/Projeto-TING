57dd9621e4e48a8b5373dc098133ef8277e4a0d9
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-08-23 22:30:18 -0300

    req 2.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        with open(path_file) as file:
+            line = file.read().splitlines()
+
+    except IOError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        return line
