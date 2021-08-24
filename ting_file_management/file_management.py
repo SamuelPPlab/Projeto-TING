6287060b2c2653ff876bb2f283b52588563dc47b
brendalima <brenda.c.lima@poli.ufrj.br>
2021-08-24 00:29:52 -0300

    requirement n2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        # tip from @carolbezerra-dev
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file, 'r',) as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(
+            "Arquivo statics/arquivo_nao_existe.txt não encontrado\n"
+            )
