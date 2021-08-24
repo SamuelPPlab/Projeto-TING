6dd9e144fe2db2cdf189cde2ce586421b503c3d2
lisvalver <lisvalver.atte@gmail.com>
2021-08-24 14:45:35 -0300

    Req 01 - Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,17 @@
 class Queue:
     def __init__(self):
-        """Open PR"""
+        self.__queueItems = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.__queueItems)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.__queueItems.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.__queueItems.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= self.__len__():
+            raise IndexError()
+        return self.__queueItems[index]
