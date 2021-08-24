8d03e10363e1c0f7800c3b540c91098dfe380047
vitorAmorims <vtamorims@gmail.com>
2021-08-24 19:38:46 -0300

    test_validar_search_by_word_com_palavra_inexistente

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,9 +1,6 @@
 def exists_word(word, instance):
     for i in instance:
-        content = ''
-        for index in i['linhas_do_arquivo'][0]:
-            content += index
-        quantity_found = content.count(word)
+        quantity_found = sum_of_occurrences(word, i)
         if quantity_found == 0:
             return []
         return [{
@@ -17,5 +14,29 @@ def exists_word(word, instance):
         }]
 
 
+def sum_of_occurrences(word, index):
+    content = ''
+    for i in index['linhas_do_arquivo'][0]:
+        content += i
+    quantity_found = content.count(word)
+    return quantity_found
+
+
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    for i in instance:
+        quantity_found = sum_of_occurrences(word, i)
+        if quantity_found == 0:
+            return []
+        for index in i['linhas_do_arquivo'][0]:
+            content = ''
+            content += index
+            return [{
+                'palavra': word,
+                'arquivo': i['nome_do_arquivo'],
+                'ocorrencias': [
+                    {
+                        'linha': sum_of_occurrences(word, index),
+                        "conteudo": content,
+                    }
+                ],
+                }]
