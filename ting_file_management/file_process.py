b1cffa5ae394bba615814476542f35a0f7f0f060
brendalima <brenda.c.lima@poli.ufrj.br>
2021-08-24 01:02:26 -0300

    requirement n4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -20,7 +20,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        dequeued_file = instance.dequeue()['nome_do_arquivo']
+        print(f"Arquivo {dequeued_file} removido com sucesso")
+    except IndexError:
+        print("Não há elementos")
 
 
 def file_metadata(instance, position):
