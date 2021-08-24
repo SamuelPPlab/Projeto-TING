4d74247dcf5fc2064a799a620cc61b3d42d53988
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-08-24 19:37:56 -0300

    req 3.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content = txt_importer(path_file)
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+
+    sys.stdout.write(f"{data}/n")
+    instance.enqueue(data)
 
 
 def remove(instance):
