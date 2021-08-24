89f7b61e4c571e66c5d533446d9fb0ef1d3350d1
brendalima <brenda.c.lima@poli.ufrj.br>
2021-08-24 00:46:38 -0300

    requirement n3?

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    txt_content = txt_importer(path_file)
+    line_counter = len(txt_content)
+
+    result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": line_counter,
+            "linhas_do_arquivo": txt_content,
+        }
+
+    instance.enqueue(result)
+
+    # tip from @luise-rios
+    sys.stdout.write(f"{result}")
 
 
 def remove(instance):
