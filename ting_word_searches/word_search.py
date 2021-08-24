e0f89e8391b59b6cd3e696f0d23e72bc234ed63a
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-08-24 13:11:49 -0300

    requisito 7 pela metade

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,4 +3,25 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    response = []
+    for n in range(len(instance)):
+        element = instance.search(n)
+        dict_response = {
+            "palavra": word,
+            "arquivo": element["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for line in element["linhas_do_arquivo"]:
+            index_word = (line.lower()).find(word.lower())
+            if index_word != -1:
+                ocorrencia = {
+                    "linha": n + 1,
+                    "conteudo": line
+                }
+                dict_response["ocorrencias"].append(ocorrencia)
+    if len(dict_response["ocorrencias"]) == 0:
+        return []
+    response.append(dict_response)
+    return response
+
+# lower https://github.com/tryber/sd-07-project-ting/pull/74/files
