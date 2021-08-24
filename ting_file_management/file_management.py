47b47be2610b12b41703d488c38a8d4e08749cfa
lisvalver <lisvalver.atte@gmail.com>
2021-08-24 15:06:40 -0300

    Req 02 - File Management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,10 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+    try:
+        return open(path_file, "r").read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
