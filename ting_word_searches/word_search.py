27ba7ff4bab1e370146aea6f4a6570bd5c6ececc
brendalima <brenda.c.lima@poli.ufrj.br>
2021-08-24 08:25:11 -0300

    requirement 6.2

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,11 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for file in range(len(instance)):
+        file_found = instance.search(file)
+        texto = file_found["linhas_do_arquivo"]
+        for palavra in texto:
+            conta = palavra.lower().count(word.lower())
+            if conta == 0:
+                return []
 
 
 def search_by_word(word, instance):
