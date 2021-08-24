d0fe8184f0ab661cf8c8d24991316a174e5bfa27
dhiegodias <ddias003@gmail.com>
2021-08-24 17:25:06 -0300

    O sonho se concretizando, vem formatura!

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,32 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if not instance.__len__():
+        content = txt_importer(path_file)
+        obj_info = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(content),
+            "linhas_do_arquivo": content
+        }
+        sys.stdout.write(f"{obj_info}\n")
+        instance.enqueue(obj_info)
+    else:
+        pass
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        return sys.stdout.write('Não há elementos\n')
+    else:
+        file_name = instance.dequeue()['nome_do_arquivo']
+        sys.stdout.write(f'Arquivo {file_name} removido com sucesso\n')
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        search = instance.search(position)
+        sys.stdout.write(f"{search}")
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,11 @@
+"""Dica fantástica do colega Tiago Esdras para alcançar os 80%, vem formatura!
+PR: https://github.com/tryber/sd-07-project-ting/blob/
+ad8f63aae750c61fbdc79543a69530783a6ed7bf/ting_word_searches/word_search.py
+"""
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    return []
 
 
 def search_by_word(word, instance):
