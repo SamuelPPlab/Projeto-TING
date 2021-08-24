c3081388cdeffe003a62309ddc6d307de373ed01
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-23 22:50:58 -0300

    feat: sixth requirements

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,29 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_found = list()
+
+    for file in range(len(instance)):
+        file_found = instance.search(file)
+        lines_found = file_found["linhas_do_arquivo"]
+
+        occurrences = []
+        for line in lines_found:
+            counter = 0
+            if word in line:
+                counter += 1
+                occurrences.append({"linha": counter})
+
+        if len(occurrences) > 0:
+            word_found.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_found["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return word_found
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+
+#
