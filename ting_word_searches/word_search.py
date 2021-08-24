01918367e30fbcac1aaf7daf50306391296184fc
CIANDT\brune <brune.freire@bradesco.com.br>
2021-08-24 19:20:32 -0300

    Vem Formatura!

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,49 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {
+                    "linha": (file["linhas_do_arquivo"].index(line) + 1)
+                    for line in file["linhas_do_arquivo"]
+                    if word.lower() in line.lower()
+                }
+            ],
+        }
+
+        if result["ocorrencias"] == [{}]:
+            pass
+        else:
+            files.append(result)
+
+    return files
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    files = []
+
+    for index in range(len(instance)):
+        file = instance.search(index)
+        result = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": [
+                {
+                    "linha": (file["linhas_do_arquivo"].index(line) + 1),
+                    "conteudo": line,
+                }
+                for line in file["linhas_do_arquivo"]
+                if word.lower() in line.lower()
+            ],
+        }
+
+        if result["ocorrencias"] == []:
+            files = []
+        else:
+            files.append(result)
+
+    return files
