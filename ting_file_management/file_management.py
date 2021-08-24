f7e7dbbe64372f4219894d2dd46e46f68fc7c9d3
PedroMarqDev <PedroMarqDev@gmail.com>
2021-08-23 21:34:01 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if (not path_file.endswith(".txt")):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file) as text_file:
+            return text_file.read().split("\n")
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
