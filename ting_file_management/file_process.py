a086352a45b1379db9fbae560e391aea97f2c9d3
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-25 00:34:02 -0300

    req 3 tá quase

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,11 +3,14 @@ import sys
 
 
 def process(path_file, instance):
-    file = txt_importer(path_file)
-    file_processed = dict()
-    file_processed["nome_do_arquivo"] = path_file
-    file_processed["qtd_linhas"] = len(file)
-    file_processed["nome_do_arquivo"] = file
+    if len(instance) < 1:
+        file = txt_importer(path_file)
+        file_processed = dict()
+        # file_processed = dict(nome_do_arquivo=path_file, qtd_linhas=len(file), linhas_do_arquivo=file)
+        file_processed['nome_do_arquivo'] = path_file
+        file_processed['qtd_linhas'] = len(file)
+        file_processed['linhas_do_arquivo'] = file
+        return file_processed
 
 
 def remove(instance):
