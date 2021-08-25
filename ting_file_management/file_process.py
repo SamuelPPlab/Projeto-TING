a71f465ab7452ce0b79d6c1cc27f880caa553ad7
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-25 00:27:03 -0300

    inicio req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,13 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    file_processed = dict()
+    file_processed["nome_do_arquivo"] = path_file
+    file_processed["qtd_linhas"] = len(file)
+    file_processed["nome_do_arquivo"] = file
 
 
 def remove(instance):
