1ae3e7e19ef5266ae41fddedaf34cbe0bacf8daa
Kesley Lahoz <namielahoz@gmail.com>
2021-08-24 21:18:30 -0300

    process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    text = txt_importer(path_file)
+    lines = len(text)
+    shape = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": lines,
+        "linhas_do_arquivo": text
+    }
+    sys.stdout.write(str(shape))
+    instance.enqueue(shape)
 
 
 def remove(instance):
