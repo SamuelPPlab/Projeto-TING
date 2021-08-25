f0527b26681eeb215e0c2ea27c9e59675b957c52
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-25 00:51:12 -0300

    req 3 func process feita

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -5,12 +5,15 @@ import sys
 def process(path_file, instance):
     if len(instance) < 1:
         file = txt_importer(path_file)
+
         file_processed = dict()
-        # file_processed = dict(nome_do_arquivo=path_file, qtd_linhas=len(file), linhas_do_arquivo=file)
+        teste = len(file)
         file_processed['nome_do_arquivo'] = path_file
         file_processed['qtd_linhas'] = len(file)
         file_processed['linhas_do_arquivo'] = file
-        return file_processed
+
+        instance.enqueue(file_processed)
+        sys.stdout.write("{file_processed}".format(file_processed=file_processed))
 
 
 def remove(instance):
