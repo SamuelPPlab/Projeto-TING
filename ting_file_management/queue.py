13b53a65b9990a7cea073bda8351ba71431a1387
Kesley Lahoz <namielahoz@gmail.com>
2021-08-24 22:22:47 -0300

    structure and proccess

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,12 +1,17 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self.queue = list()
+        self.INITIAL_INDEX = 0
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        if value not in self.queue:
+            self.queue.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
