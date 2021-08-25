dce9bb6c1736892de2a4a50aee519dcb49e3eed7
lisvalver <lisvalver.atte@gmail.com>
2021-08-25 09:10:26 -0300

    Req 04 - Word Search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,41 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = []
+    for file in range(instance.__len__()):
+        result = {
+            "palavra": word,
+            "arquivo": instance.search(file)["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        file_lines = instance.search(file)["linhas_do_arquivo"]
+        for index, line in enumerate(file_lines):
+            if word.lower() in line.lower():
+                result["ocorrencias"].append({"linha": index + 1})
+
+        if result["ocorrencias"].__len__() != 0:
+            search_result.append(result)
+
+    return search_result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = []
+    for file in range(instance.__len__()):
+        result = {
+            "palavra": word,
+            "arquivo": instance.search(file)["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        file_lines = instance.search(file)["linhas_do_arquivo"]
+        for index, line in enumerate(file_lines):
+            if word.lower() in line.lower():
+                result["ocorrencias"].append(
+                    {
+                        "linha": index + 1,
+                        "conteudo": line,
+                    }
+                )
+
+        if result["ocorrencias"].__len__() != 0:
+            search_result.append(result)
+
+    return search_result
