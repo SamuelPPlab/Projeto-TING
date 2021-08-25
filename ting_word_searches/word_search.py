9381e7842f78ca2e71442383f42c5916e4f489f0
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-25 02:18:08 -0300

    PRECISO dormir aprovada

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,6 +3,7 @@ def exists_word(word, instance):
     file_name = file['nome_do_arquivo'].lower()
     if word.lower() in file_name:
         dict_babado = dict()
+        return dict_babado
     else:
         return []
 
