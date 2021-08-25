1a94e4989ccfdaa6678287d2203af822b4e5f269
Lucas Nonato <lucas.nonato1@hotmail.com>
2021-08-24 23:47:41 -0300

    req 2 sem o tratamento na string

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    try:
+        with open(path_file) as file:
+            text = file.read()
+            return text
+    except FileNotFoundError:
+        return sys.stderr.write('Arquivo {path_file} não encontrado\n'.format(path_file=path_file))
+        # Template Literal
