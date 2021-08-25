e6a3ade6820e033afc31212b3323b56c6ee0f098
wberilo <wberilo@gmail.com>
2021-08-25 11:36:17 -0300

    req 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,21 @@
 class Queue:
+    FIRST_ELEMENT = 0
+
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self._data.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self._data):
+            value = self._data[index]
+            return value
+        else:
+            raise IndexError
