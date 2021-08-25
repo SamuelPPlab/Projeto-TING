fe7711569c4e05d9cd75d67e70dc46ba7a0a88f4
Kesley Lahoz <namielahoz@gmail.com>
2021-08-24 21:56:32 -0300

    lint fix

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,18 +1,17 @@
-import sys
 from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
     """Aqui irá sua implementação"""
-    text = txt_importer(path_file)
-    lines = len(text)
     shape = {
         "nome_do_arquivo": path_file,
-        "qtd_linhas": lines,
-        "linhas_do_arquivo": text
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
     }
-    sys.stdout.write(str(shape))
+
     instance.enqueue(shape)
+
+    print(shape)
 
 
 def remove(instance):
