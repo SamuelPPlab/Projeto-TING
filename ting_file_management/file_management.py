1efa0f4cfec26869676c3d98da26e84aee4cdcdc
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-08-24 23:42:39 -0300

    2nd req

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,15 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    # só para o PR
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido')
+
+    if not os.path.isfile(path_file):
+        # https://www.geeksforgeeks.org/python-os-path-isfile-method/
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+    with open(path_file) as file:
+        # https://www.w3schools.com/python/ref_string_splitlines.asp
+        return file.read().splitlines()
