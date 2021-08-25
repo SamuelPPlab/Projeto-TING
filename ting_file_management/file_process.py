ec02975dc7215dfbdc3ec425e6cbba5501c446d7
flaviosoliver <flavsoliver@gmail.com>
2021-08-24 23:58:11 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,17 @@
+import sys
+
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_content = txt_importer(path_file)
+    processed_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+    instance.enqueue(processed_data)
+    sys.stdout.write(str(processed_data))
 
 
 def remove(instance):
