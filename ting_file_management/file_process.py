3ec26649ed8548d7bca46fca557c7bc229eb644c
wberilo <wberilo@gmail.com>
2021-08-25 11:41:46 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+    file_text = txt_importer(path_file)
+    content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_text),
+        "linhas_do_arquivo": file_text, }
+    sys.stdout.write(f"{content}")
+    instance.enqueue(content)
 
 
 def remove(instance):
