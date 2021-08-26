b816bba932b3f7b47f1d2ceb8c90b790f0d16347
Lucas Staroscky <LucasStaroscky@MacBook-Pro.local>
2021-08-26 12:39:19 -0300

    requisito 1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(file_path):
+    try:
+        assert file_path.endswith(".txt")
+        with open(file_path, "r") as file:
+            contents = file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {file_path} não encontrado\n")
+    except AssertionError:
+        sys.stderr.write("Formato inválido\n")
+    else:
+        return contents
