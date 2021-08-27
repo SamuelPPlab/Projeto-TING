f0201ace1348004cede5da276733276ef15cecaa
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-08-27 18:55:00 -0300

    feat: requirement 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -8,22 +8,41 @@ def exists_word(word, instance):
         occurrences = []
         for line in lines_found:
             counter = 0
-            if word in line:
+            if word.lower() in line.lower():
                 counter += 1
                 occurrences.append({"linha": counter})
 
-        if len(occurrences) > 0:
-            word_found.append(
-                {
-                    "palavra": word,
-                    "arquivo": file_found["nome_do_arquivo"],
-                    "ocorrencias": occurrences,
-                }
-            )
+            if len(occurrences) > 0:
+                word_found.append(
+                    {
+                        "palavra": word,
+                        "arquivo": file_found["nome_do_arquivo"],
+                        "ocorrencias": occurrences,
+                    }
+                )
     return word_found
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_found = list()
 
-#
+    for file in range(len(instance)):
+        file_found = instance.search(file)
+        lines_found = file_found["linhas_do_arquivo"]
+
+        occurrences = []
+        for line in lines_found:
+            counter = 0
+            if word.lower() in line.lower():
+                counter += 1
+                occurrences.append({"linha": counter, "conteudo": line})
+
+            if len(occurrences) > 0:
+                word_found.append(
+                    {
+                        "palavra": word,
+                        "arquivo": file_found["nome_do_arquivo"],
+                        "ocorrencias": occurrences,
+                    }
+                )
+    return word_found
