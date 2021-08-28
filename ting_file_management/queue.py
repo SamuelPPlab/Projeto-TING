46b5acabe51ae025b0f8115a514cd8c971cee682
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-28 17:30:44 -0300

    feat: Queue class FIFO, and search by index

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,21 @@
 class Queue:
+    FIRST_ELEMENT = 0
+
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.insert(self.FIRST_ELEMENT, value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self._data:
+            return self._data.pop()
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if self._data:
+            return self._data[len(self._data) - 1 - index]
+        raise(IndexError)
