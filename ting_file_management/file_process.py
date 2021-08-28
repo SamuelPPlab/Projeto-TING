fd045d42dd2b261bdbfb364a9aa985bf16199354
Carlos Souza <carlos.pessoal@hotmail.com>
2021-08-28 18:10:22 -0300

    Terceiro, quarto e quinto requisito

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if (path_file == instance.search(index)['nome_do_arquivo']):
+            return
+
+    file = txt_importer(path_file)
+    file_format = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file),
+        'linhas_do_arquivo': file,
+    }
+
+    instance.enqueue(file_format)
+    print(file_format)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if (len(instance) == 0):
+        print('Não há elementos')
+        return
+
+    file = instance.dequeue()
+    filename = file['nome_do_arquivo']
+    print(f'Arquivo {filename} removido com sucesso')
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return instance.search(position)
+    except IndexError:
+        print('Posição inválida', file=sys.stderr)
