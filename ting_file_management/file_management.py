0c8796a58593909323d8583f0132f0a2df932546
Luiz Mariz <lfsmariz@gmail.com>
2021-08-28 13:59:51 -0300

    go 100%

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, mode="r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(
+            "Arquivo statics/arquivo_nao_existe.txt não encontrado",
+            file=sys.stderr,
+        )
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(len(instance)):
+        if path_file == instance.search(i).get("nome_do_arquivo"):
+            return
+
+    lines = txt_importer(path_file)
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines,
+    }
+
+    instance.enqueue(file_info)
+    print(file_info)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos")
+
+    removed = instance.dequeue()
+    removed_filename = removed.get("nome_do_arquivo")
+
+    print(f"Arquivo {removed_filename} removido com sucesso")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return instance.search(position)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
