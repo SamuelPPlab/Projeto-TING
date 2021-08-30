c619fea3cce06ad7aae8593be72cc97054bdb25a
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-30 20:25:31 -0300

    feat: process file, print report, instanciate file

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_report = {
+        'nome_do_arquivo': '',
+        'qtd_linhas': '',
+        'linhas_do_arquivo': ''
+    }
+    for i in range(len(instance)):
+        if instance.search(i) == path_file:
+            return False
+    file_report['nome_do_arquivo'] = path_file
+    file_report['linhas_do_arquivo'] = txt_importer(path_file)
+    with open(path_file, 'r', newline='\n') as file:
+        file_report["qtd_linhas"] = len(file.readlines())
+    instance.enqueue(path_file)
+    print(file_report)
+    return True
 
 
 def remove(instance):
