0861c02dd1dc0546e3d05f976d03552ad4c8f4be
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-30 20:24:53 -0300

    checkint requirement 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    if path_file.split('.')[1] != 'txt':
+        sys.stderr.write("Formato inválido\n")
+
+    try:
+        fd = open(path_file, 'r', encoding="utf8")  # fd = file data
+        data = fd.read().splitlines()
+        return data
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
