a034335fe9c2e166bb2ed2eca5109fba8b94d157
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-30 12:25:56 -0300

    fix: deal with err via stderr

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    extension = path_file.split('.', -1)[-1]
+    if extension != 'txt':
+        sys.stderr.write("Formato inválido\n")
+        return None
+    try:
+        lines = ''
+        with open(path_file, 'r', newline='\n') as file:
+            lines = file.read()
+        file.close()
+        return lines.split('\n')
+    except(FileNotFoundError):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
