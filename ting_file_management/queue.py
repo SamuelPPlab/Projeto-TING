c26309909c5747aef1b65fb2b6d2494457231fbd
Macarena Rodriguez <macarenar697@gmail.com>
2021-08-29 22:28:14 -0300

    Req 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,23 @@
 class Queue:
+    FIRST_ELEMENT = 0
+
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        value = self._data[0]
+        del self._data[0]
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        print(index)
+        if self._data[index] and index >= 0:
+            return self._data[index]
+        else:
+            raise IndexError
