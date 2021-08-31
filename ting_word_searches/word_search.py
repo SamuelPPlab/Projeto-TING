d3cdc676ce01f1685bcb2a227d4ccace29ac84ea
vladimirrchagas <vladimirrchagas@gmail.com>
2021-08-31 20:00:56 -0300

    checking requirements 6-7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,45 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
+    index = 0
+    exists = []
+
+    while index < instance.__len__():
+        file = instance.search(index)
+        content = file["linhas_do_arquivo"]
+        exists_in_file = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for line in content:
+            if word.lower() in line.lower():
+                exists_in_file["ocorrencias"].append({"linha": index + 1})
+        if len(exists_in_file["ocorrencias"]):
+            exists.append(exists_in_file)
+        index += 1
+    return exists
 
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    index = 0
+    exists = []
+
+    while index < instance.__len__():
+        file = instance.search(index)
+        content = file["linhas_do_arquivo"]
+        exists_in_file = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for line in content:
+            if word.lower() in line.lower():
+                exists_in_file["ocorrencias"].append({
+                    "linha": index + 1,
+                    "conteudo": line
+                })
+        if len(exists_in_file["ocorrencias"]):
+            exists.append(exists_in_file)
+        index += 1
+    return exists
