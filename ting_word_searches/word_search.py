4802ad2d75adea3167ac3fa448c8f2ba5450fcb4
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-31 18:03:44 -0300

    feat: check word existance

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,31 @@
+from ting_file_management.file_management import txt_importer
+from ting_file_management.file_process import get_file_report
+
+
+def get_lines_with_word(lines, word):
+    ocurrencies = []
+    for index in range(len(lines)):
+        if word.lower() in lines[index].lower():
+            ocurrencies.append({'linha': index + 1})
+    return ocurrencies or False
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word_search_report = {}
+    for i in range(len(instance)):
+        file_report = get_file_report(instance.search(i), instance)
+        file_lines = list(txt_importer(file_report["nome_do_arquivo"]))
+        match = get_lines_with_word(file_lines, word)
+
+        if not match:
+            return []
+
+        word_search_report = [{
+            "palavra": word,
+            "arquivo": file_report["nome_do_arquivo"],
+            "ocorrencias": match
+        }]
+        return word_search_report
 
 
 def search_by_word(word, instance):
