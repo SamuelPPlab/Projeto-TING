e10d560949d499b77368e0c7e83e9c7429fc7d73
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-31 17:40:25 -0300

    refactor: apart report and process (check instance)

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,22 +1,16 @@
 import sys
 from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
 
 
 def process(path_file, instance):
-    file_report = {
-        'nome_do_arquivo': '',
-        'qtd_linhas': '',
-        'linhas_do_arquivo': ''
-    }
     for i in range(len(instance)):
         if instance.search(i) == path_file:
+            print(instance.search(i))
             return False
-    file_report['nome_do_arquivo'] = path_file
-    file_report['linhas_do_arquivo'] = txt_importer(path_file)
-    with open(path_file, 'r', newline='\n') as file:
-        file_report["qtd_linhas"] = len(file.readlines())
-    instance.enqueue(path_file)
-    print(file_report)
+    queue = Queue()
+    queue.enqueue(path_file)
+    print(get_file_report(path_file, instance))
     return True
 
 
@@ -36,3 +30,17 @@ def file_metadata(instance, position):
         return True
     except(IndexError):
         sys.stderr.write('Posição inválida')
+
+
+def get_file_report(path_file, instance):
+    file_report = {
+        'nome_do_arquivo': '',
+        'qtd_linhas': '',
+        'linhas_do_arquivo': ''
+    }
+    file_report['nome_do_arquivo'] = path_file
+    file_report['linhas_do_arquivo'] = txt_importer(path_file)
+    with open(path_file, 'r', newline='\n') as file:
+        file_report["qtd_linhas"] = len(file.readlines())
+    instance.enqueue(path_file)
+    return file_report
