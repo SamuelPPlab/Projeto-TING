a23e85712a4d6d0765eae6c13d64664e8bdee2f2
vicmafe <victormateuslf@gmail.com>
2021-08-31 17:17:12 -0300

    create file queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,24 @@
+import sys
+
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.my_queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.my_queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.my_queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.my_queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0:
+            raise IndexError
+        try:
+            return self.my_queue[index]
+        except IndexError:
+            print(file=sys.stderr)
+            raise
