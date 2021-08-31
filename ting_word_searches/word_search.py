74d2eb791c7595324ef9fde0eef6b43c7a070581
Pedro Risso <pmerchiorattor@gmail.com>
2021-08-31 18:34:51 -0300

    feat: generate report with line content

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -2,31 +2,40 @@ from ting_file_management.file_management import txt_importer
 from ting_file_management.file_process import get_file_report
 
 
-def get_lines_with_word(lines, word):
+def get_lines_with_word(lines, word, content):
     ocurrencies = []
     for index in range(len(lines)):
         if word.lower() in lines[index].lower():
-            ocurrencies.append({'linha': index + 1})
+            if not content:
+                ocurrencies.append({'linha': index + 1})
+            else:
+                ocurrencies.append({
+                    'linha': index + 1,
+                    'conteudo': lines[index]
+                    })
     return ocurrencies or False
 
 
-def exists_word(word, instance):
+def generate_report(word, instance, content=False):
     word_search_report = []
     for i in range(len(instance)):
         file_report = get_file_report(instance.search(i), instance)
         file_lines = list(txt_importer(file_report["nome_do_arquivo"]))
-        match = get_lines_with_word(file_lines, word)
+        match = get_lines_with_word(file_lines, word, content)
 
-        if not match:
-            return []
-        word_search_report.append({
-            "palavra": word,
-            "arquivo": file_report["nome_do_arquivo"],
-            "ocorrencias": match
-        })
+        if match:
+            word_search_report.append({
+                "palavra": word,
+                "arquivo": file_report["nome_do_arquivo"],
+                "ocorrencias": match
+            })
 
-    return word_search_report
+    return word_search_report or []
+
+
+def exists_word(word, instance):
+    return generate_report(word, instance)
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    return generate_report(word, instance, True)
