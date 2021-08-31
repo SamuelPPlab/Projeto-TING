4c41179bfbf2de674bf9296b286e4deb6f3b0d94
vicmafe <victormateuslf@gmail.com>
2021-08-31 17:57:06 -0300

    import txt news

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,3 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
-    """go to the last!"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+
+    try:
+        with open(path_file) as file:
+            return [line.replace("\n", "") for line in file]
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
