e4e0b25a7d6dbe0267fb55d96e7e9c4348ca5cd7
vicmafe <victormateuslf@gmail.com>
2021-09-01 22:06:06 -0300

    process file done

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if len(instance) < 1:
+        file = txt_importer(path_file)
+        resuslt = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+
+    sys.stdout.write(str(resuslt))
+    instance.enqueue(resuslt)
 
 
 def remove(instance):
