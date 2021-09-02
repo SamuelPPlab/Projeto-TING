4e44f2463a0e91a67cde013e7d7ef931caee44ee
vicmafe <victormateuslf@gmail.com>
2021-09-01 22:41:58 -0300

    remove file and file metadata done

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -11,13 +11,22 @@ def process(path_file, instance):
             "linhas_do_arquivo": file
         }
 
-    sys.stdout.write(str(resuslt))
-    instance.enqueue(resuslt)
+        sys.stdout.write(str(resuslt))
+        instance.enqueue(resuslt)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) <= 0:
+        return sys.stdout.write("Não há elementos\n")
+
+    file_removed = instance.dequeue()["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {file_removed} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file = instance.search(position)
+        return file
+
+    except IndexError:
+        sys.stderr.write("Posição inválida\n")
