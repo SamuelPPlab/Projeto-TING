d10ea3ac8524935c1bcb47071deb6e2999847e19
SamuelPPlab <samuelpp.s@hotmail.com>
2021-09-03 09:38:04 -0300

    requisito 3 pronto

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,17 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
-
+    if not len(instance):
+        new_file = txt_importer(path_file)
+        info = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len(new_file),
+            'linhas_do_arquivo': new_file,
+        }
+        instance.enqueue(info)
+        sys.stdout.write(f"{info}")
 
 def remove(instance):
     """Aqui irá sua implementação"""
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -2,24 +2,24 @@ class Queue:
     first_element = 0
     def __init__(self):
         """Inicialize sua estrutura aqui"""
-        self._data = []
+        self.data = []
 
     def __len__(self):
-        return len(self._data)
+        return len(self.data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
-        self._data.append(value)
+        self.data.append(value)
     def dequeue(self):
         """Aqui irá sua implementação"""
-        if self._data:
-            return self._data.pop(self.first_element)
+        if self.data:
+            return self.data.pop(self.first_element)
         return None
 
     def search(self, index):
         """Aqui irá sua implementação"""
         if index < 0:
             raise IndexError
-        if self._data[index]:
-            return self._data[index]
+        if self.data[index]:
+            return self.data[index]
         raise IndexError
