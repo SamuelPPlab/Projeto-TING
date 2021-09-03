c1c55edf39bc5885de365653e34e1a487ed0baac
Lets <letsferreiratx@gmail.com>
2021-09-02 22:07:52 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,15 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+import sys
 
+def process(path_file, instance):
+    content_file = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content_file),
+        "linhas_do_arquivo": content_file
+    }
+    sys.stdout.write(str(new_dict))
+    instance.enqueue(new_dict)
 
 def remove(instance):
     """Aqui irá sua implementação"""
