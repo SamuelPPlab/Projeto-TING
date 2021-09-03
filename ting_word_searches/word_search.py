94189db851c63157ded52205babf9e1012c5c89d
SamuelPPlab <samuelpp.s@hotmail.com>
2021-09-03 11:27:04 -0300

    requisito 7 pronto

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,3 +19,16 @@ def exists_word(word, instance):
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
+    data = instance.data
+    results = []
+    for item in data:
+        placeholder = { 'arquivo': item['nome_do_arquivo'], 'ocorrencias': [], 'palavra': word }
+        for index, linha in enumerate(item['linhas_do_arquivo']):
+            lower_l = linha.lower()
+            lower_w = word.lower()
+            if lower_w in lower_l:
+                placeholder['ocorrencias'].append({'linha': index + 1, 'conteudo': linha})
+        results.append(placeholder)
+    if len(results[0]['ocorrencias']) > 0:
+        return results
+    return []
