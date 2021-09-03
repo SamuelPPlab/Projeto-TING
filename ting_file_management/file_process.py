c3700fa65c98aefc6c6c102ec2509d44176e41a5
vini-lima <vini20.henrique@gmail.com>
2021-09-02 22:56:54 -0300

    Requisitos 3, 4 e 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,31 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if not len(instance):
+        file = txt_importer(path_file)
+        content = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+        instance.enqueue(content)
+        sys.stdout.write(f"{content}")
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return sys.stdout.write("Não há elementos\n")
+
+    path = instance.search(0)["nome_do_arquivo"]
+    return print(
+        f'Arquivo {path} removido com sucesso'
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        return instance.search(position)
+    except IndexError:
+        sys.stderr.write('Posição inválida')
