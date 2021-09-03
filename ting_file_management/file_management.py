b5bc979705075eed2e91a84a3cfb06951d6e4aae
vini-lima <vini20.henrique@gmail.com>
2021-09-02 22:17:36 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return sys.stderr.write("Formato inválido\n")
+
+    if not os.path.isfile(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    with open(path_file, "r") as file:
+        return file.read().splitlines()
