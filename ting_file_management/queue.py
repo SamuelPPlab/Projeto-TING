1b930930cbb905ab9f153e7ce044210d204860d1
Lets <letsferreiratx@gmail.com>
2021-09-02 21:51:55 -0300

    req1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,22 @@
+
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
+        self.START = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        if value not in self.queue:
+            self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.queue:
+            return self.queue.pop(self.START)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < self.START or index > len(self) - 1:
+            raise IndexError()
+        return self.queue[index]
