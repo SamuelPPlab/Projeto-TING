6ba5528aa84a1c8779d62d2ebad0591aeb7c4113
Leandro <leandro.feitoza@gmail.com>
2021-09-03 22:23:03 -0300

    passando todos local

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,50 @@
+# Idéia do PR do colega Carlos Souza
+# https://github.com/tryber/sd-07-project-ting/blob/carlos-souza-project-ting/ting_word_searches/word_search.py
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_word_list = []
+    for i in range(len(instance)):
+        file = instance.search(i)
+        file_name = file["nome_do_arquivo"]
+        file_lines = file["linhas_do_arquivo"]
+        found_in_file_list = []
+
+        for line_index, line in enumerate(file_lines):
+            if word.lower() in line.lower():
+                found_in_file_list.append({"linha": line_index + 1})
+
+        if len(found_in_file_list) != 0:
+            found_word_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": found_in_file_list,
+                }
+            )
+
+        return found_word_list
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_word_list = []
+    for index in range(len(instance)):
+        file = instance.search(index)
+        file_name = file["nome_do_arquivo"]
+        file_lines = file["linhas_do_arquivo"]
+        found_in_file_list = []
+
+        for line_index, line in enumerate(file_lines):
+            if word.lower() in line.lower():
+                found_in_file_list.append(
+                    {"linha": line_index + 1, "conteudo": line}
+                )
+
+        if len(found_in_file_list) != 0:
+            found_word_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": found_in_file_list,
+                }
+            )
+
+    return found_word_list
