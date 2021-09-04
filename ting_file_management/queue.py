57d54375b136ff5911ad8e01766e81d834f24e78
josielvs <josielvs@gmail.com>
2021-09-04 09:59:57 -0300

    Requisito 2 - File Management

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,26 @@
 class Queue:
     def __init__(self):
         """Inicialize sua estrutura aqui"""
+        self._data = list()
 
     def __len__(self):
         """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
         """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
+        is_value = self._data[0]
+        del self._data[0]
+        return is_value
 
     def search(self, index):
         """Aqui irá sua implementação"""
+        print(index)
+        if self._data[index] and index >= 0:
+            return self._data[index]
+        else:
+            raise IndexError
